6f04cfa15878891e5f4ce504f3abf3e78cfa561d
Andreas Gangsø <andreasgangso@gmail.com>
2021-10-27 15:48:27 +0200

    edit media_collectable view

--- db/migrations/20211027114000_media_view.up.sql
+++ db/migrations/20211027114000_media_view.up.sql
@@ -4,7 +4,12 @@ SELECT
     c.type,
     c.available_from,
     c.available_to,
+	t.title,
+	t.description,
+	t.long_description,
+	t.image_id,
+	t.id as translation_id,
     m.*
 FROM media m
 JOIN collectable c USING (id)
-JOIN media_t t ON t.media_id = m.id AND language_code = 'no'
+LEFT JOIN media_t t ON t.media_id = m.id AND language_code = 'no'
