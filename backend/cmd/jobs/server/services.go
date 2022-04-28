d440953b342a3c824c471e07abc6cdb856fdc0a4
Matjaz Debelak <matjaz@debelak.org>
2022-04-28 15:24:52 +0200

    Testing GQL

--- backend/cmd/jobs/server/services.go
+++ backend/cmd/jobs/server/services.go
@@ -3,18 +3,25 @@ package server
 import (
 	"database/sql"
 
+	"github.com/aws/aws-sdk-go/service/mediapackagevod"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/bcc-code/brunstadtv/backend/sqlc"
 )
 
 // ExternalServices used by the server
 type ExternalServices struct {
-	RawDB    *sql.DB
-	DB       *sqlc.Queries
-	S3Client *s3.S3
+	RawDB           *sql.DB
+	DB              *sqlc.Queries
+	S3Client        *s3.S3
+	MediaPackageVOD *mediapackagevod.MediaPackageVod
 }
 
 // GetS3Client as stored in the struct
 func (e ExternalServices) GetS3Client() *s3.S3 {
 	return e.S3Client
+}
+
+// GetMediaPackageVOD as stored in the struct
+func (e ExternalServices) GetMediaPackageVOD() *mediapackagevod.MediaPackageVod {
+	return e.MediaPackageVOD
 }
