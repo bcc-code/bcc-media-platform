d5e569e369ec33b378d718a1fb38a6824cc0fb01
Matjaz Debelak <matjaz@debelak.org>
2022-04-28 11:59:12 +0200

    Restructure some things
    Read (most of) the config from the env
    Create services when creating a new server

--- backend/cmd/jobs/server/server.go
+++ backend/cmd/jobs/server/server.go
@@ -1,16 +1,12 @@
 package server
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 	"net/http"
 
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/bcc-code/brunstadtv/backend/asset"
 	"github.com/bcc-code/brunstadtv/backend/events"
 	"github.com/bcc-code/brunstadtv/backend/pubsub"
-	"github.com/bcc-code/brunstadtv/backend/sqlc"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/gin-gonic/gin"
 )
@@ -19,32 +15,25 @@ var (
 	errUndefinedHandler = fmt.Errorf("Handler for this message type is not defined")
 )
 
-type eventHandler func(context.Context, cloudevents.Event) error
-
 // NewServer returns a new server for handling the HTTP requests
 // Yes, go, I know it's "annoying to work with" but in this case you will have to deal with it
-func NewServer(db *sql.DB) *server {
+func NewServer(s ExternalServices, c ConfigData) *server {
 	return &server{
-		rawDB:      db,
-		db:         sqlc.New(db),
-		awsSession: session.Must(session.NewSession()),
-		eventHandlers: map[string]eventHandler{
-			events.TypeAssetDelivered: asset.Ingest,
-		},
+		services: s,
+		config:   c,
 	}
 }
 
 // Server is the base for all HTTP handler
 type server struct {
-	rawDB      *sql.DB
-	db         *sqlc.Queries
-	awsSession *session.Session
-
-	eventHandlers map[string]eventHandler
+	services ExternalServices
+	config   ConfigData
 }
 
 // IngestVod processes the message for ingesting a VOD asset
 func (s server) ProcessMessage(c *gin.Context) {
+	ctx := c.Request.Context()
+
 	msg, err := pubsub.MessageFromCtx(c)
 	if err != nil {
 		// TODO
@@ -60,9 +49,10 @@ func (s server) ProcessMessage(c *gin.Context) {
 		return
 	}
 
-	if f, ok := s.eventHandlers[e.Type()]; ok {
-		err = f(c.Request.Context(), e)
-	} else {
+	switch e.Type() {
+	case events.TypeAssetDelivered:
+		err = asset.Ingest(ctx, s.services, s.config, e)
+	default:
 		err = errUndefinedHandler
 	}
 
