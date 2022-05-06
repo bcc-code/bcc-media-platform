0216e20f7381270a309a2d6fcfaa7aac279e87e8
Andreas Gangsø <andreasgangso@gmail.com>
2022-05-06 13:56:28 +0200

    feat: typescript setup

--- cms/extensions/hooks/crowdin-sync/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var e=({filter:e,action:t})=>{e("items.create",((...e)=>{console.log("Creating Item!"),console.log(e)})),t("items.create",(()=>{console.log("Item created!")}))};module.exports=e;
