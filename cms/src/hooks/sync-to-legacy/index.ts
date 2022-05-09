80200c2e91b398ca7a526e0106130251d3f8c97e
Andreas <andreasgangso@gmail.com>
2022-05-09 15:58:33 +0200

    wip: sync back to legacy

--- cms/src/hooks/sync-to-legacy/index.ts
+++ cms/src/hooks/sync-to-legacy/index.ts
@@ -2,8 +2,8 @@ import { defineHook } from '@directus/extensions-sdk';
 import { ItemsService } from 'directus';
 import episodes from '../../btv'
 import knex from 'knex'
-import { EpisodeEntity } from '@/Database';
-import { getStatusFromNew } from './utils';
+import { EpisodeEntity, LanguageEntity, LocalizedStringEntity, SeriesEntity } from '@/Database';
+import { createLocalizable, getStatusFromNew, objectPatch, upsertLS } from './utils';
 
 var oldKnex = knex({
     client: 'mssql',
@@ -26,37 +26,252 @@ function isObjectUseless(patch: object) {
 } 
 
 export default defineHook(({ filter, action }, {services}) => {    
-	filter('items.update', async (p,m,c) =>  {
-        let input = p as Partial<episodes.components["schemas"]["ItemsEpisodes"]>
-        if(m.collection != "episodes") {
+	action('items.update', async (m,c) =>  {
+		console.log('Item updated!');
+		console.log(m);
+        if (m.collection != "episodes") {
             return
         }
-		console.log('Updating Item!');
-		console.log(p);
-		console.log(m);
         // get legacy id
 		const itemsService = new ItemsService<episodes.components["schemas"]["ItemsEpisodes"]>("episodes", {
 			knex: c.database as any,
 			schema: c.schema,
 		});
-        let e = await itemsService.readOne(Number(m.keys[0]))
+        let e = await itemsService.readOne(Number(m.keys[0]), { fields: ['*.*.*'] })
+        let season = e.season_id as episodes.components["schemas"]["ItemsSeasons"]
+        let asset = e.asset_id as episodes.components["schemas"]["ItemsAssets"]
+        let image = e.image_file_id as episodes.components["schemas"]["Files"]
+        console.log("directus", e)
 
         // update it in original 
         let patch: Partial<EpisodeEntity> = {
-            EpisodeNo: input.episode_number,
-            Published: input.publish_date as unknown as Date,
-            AvailableTo: input.available_to as unknown as Date,
-            AvailableFrom: input.available_from as unknown as Date,
-            Status: getStatusFromNew(input.status)
+            VideoId: asset?.legacy_id ?? 1041, // TODO: remove this temp id
+            SeasonId: season.legacy_id,
+            EpisodeNo: e.episode_number,
+            Published: e.publish_date as unknown as Date,
+            AvailableTo: e.available_to as unknown as Date,
+            AvailableFrom: e.available_from as unknown as Date,
+            Status: getStatusFromNew(e.status)
         }
+
+        if (image != null) {
+            patch.Image = "https://brunstadtv.imgix.net/"+image.filename_disk
+        }
+
+        if (e.download_usergroups.some(ug => (ug as any).usergroups_code.code === "fktb-download" || (ug as any).usergroups_code.code === "fktb-early-access")) {
+            patch.AllowSpecialAccessFKTB = true
+        }
+        if (e.earlyaccess_usergroups.some(ug => (ug as any).usergroups_code.code === "kids-early-access")) {
+            patch.AllowSpecialAccess = true
+        }
+
+        if (e.usergroups.some(ug => (ug as any).usergroups_code.code === "public") && e.usergroups.some(ug => (ug as any).usergroups_code.code === "bcc-members")) {
+            patch.Visibility = 3
+        } else if (e.usergroups.some(ug => (ug as any).usergroups_code.code === "public")) {
+            patch.Visibility = 2
+        } else {
+            patch.Visibility = 1
+        }
+
+        if (e.status == "published") {
+            patch.Status = 1
+        } else {
+            patch.Status = 0
+        }
+
+        let languages = await oldKnex<LanguageEntity>("language").select("*")
         
+        for (let t of e.translations) {
+            t = t as episodes.components["schemas"]["ItemsEpisodesTranslations"]
+            let lang = (t.languages_code as episodes.components["schemas"]["ItemsLanguages"])    
+            if (lang.code != "no") {
+                // We want original to be source of truth for translations for now
+                continue
+            }
+            console.log("updating norwegian")
+            let oldLang = languages.find(l => l.CultureCode == lang.code)
+            await upsertLS(oldKnex, t.legacy_title_id, oldLang, t.title)
+            await upsertLS(oldKnex, t.legacy_description_id, oldLang, t.description)
+            await upsertLS(oldKnex, t.legacy_extra_description_id, oldLang, t.extra_description)
+            console.log("done updating norwegian")
+        }
+
+        console.log("patch", patch)
         if (!isObjectUseless(patch)) {
             let a = await oldKnex<EpisodeEntity>("episode").where("id", e.legacy_id).update(patch).returning("*")
             console.log(a)
         }
 	});
 
-	action('items.create', () => {
+	action('items.create', async (m, c) => {
 		console.log('Item created!');
+		console.log(m);
+        if (m.collection != "episodes") {
+            return
+        }
+        // get legacy id
+		const episodesService = new ItemsService<episodes.components["schemas"]["ItemsEpisodes"]>("episodes", {
+			knex: c.database as any,
+			schema: c.schema,
+		});
+        const episodeTranslationsService = new ItemsService<episodes.components["schemas"]["ItemsEpisodesTranslations"]>("episodes_translations", {
+			knex: c.database as any,
+			schema: c.schema,
+		});
+        let e = await episodesService.readOne(Number(m.key), { fields: ['*.*.*'] })
+        let season = e.season_id as episodes.components["schemas"]["ItemsSeasons"]
+        let asset = e.asset_id as episodes.components["schemas"]["ItemsAssets"]
+        let image = e.image_file_id as episodes.components["schemas"]["Files"]
+        console.log("directus", e)
+
+        // update it in original 
+        let patch: Partial<EpisodeEntity> = {
+            VideoId: asset?.legacy_id ?? 1041, // TODO: remove this temp id
+            SeasonId: season.legacy_id,
+            EpisodeNo: e.episode_number,
+            Published: e.publish_date as unknown as Date,
+            AvailableTo: e.available_to as unknown as Date,
+            AvailableFrom: e.available_from as unknown as Date,
+            Status: getStatusFromNew(e.status),
+            LastUpdate: e.date_created as unknown as Date
+        }
+
+        if (image != null) {
+            patch.Image = "https://brunstadtv.imgix.net/"+image.filename_disk
+        }
+
+        if (e.download_usergroups.some(ug => (ug as any).usergroups_code.code === "fktb-download" || (ug as any).usergroups_code.code === "fktb-early-access")) {
+            patch.AllowSpecialAccessFKTB = true
+        }
+        if (e.earlyaccess_usergroups.some(ug => (ug as any).usergroups_code.code === "kids-early-access")) {
+            patch.AllowSpecialAccess = true
+        }
+
+        if (e.usergroups.some(ug => (ug as any).usergroups_code.code === "public") && e.usergroups.some(ug => (ug as any).usergroups_code.code === "bcc-members")) {
+            patch.Visibility = 3
+        } else if (e.usergroups.some(ug => (ug as any).usergroups_code.code === "public")) {
+            patch.Visibility = 2
+        } else {
+            patch.Visibility = 1
+        }
+
+        if (e.status == "published") {
+            patch.Status = 1
+        } else {
+            patch.Status = 0
+        }
+
+        let languages = await oldKnex<LanguageEntity>("language").select("*")
+        
+        for (let t of e.translations) {
+            t = t as episodes.components["schemas"]["ItemsEpisodesTranslations"]
+            let lang = (t.languages_code as episodes.components["schemas"]["ItemsLanguages"])    
+            if (lang.code != "no") {
+                // We want original to be source of truth for translations for now
+                continue
+            }
+            console.log("adding norwegian")
+            console.log("t:", t)
+            let oldLang = languages.find(l => l.CultureCode == lang.code)
+            patch.TitleId = await createLocalizable(oldKnex)
+            patch.DescriptionId = await createLocalizable(oldKnex)
+            patch.LongDescriptionId = await createLocalizable(oldKnex)
+            console.log(patch)
+            //let ids = await ensureLocalizablesExist(oldKnex, 'episode', patch, {'title': 'legacy_title_id', 'description': 'legacy_description_id', 'long_description': 'legacy_extra_description_id'})
+            
+            t = {
+                ...t,
+                legacy_title_id: patch.TitleId,
+                legacy_description_id: patch.DescriptionId,
+                legacy_extra_description_id: patch.LongDescriptionId
+            }
+            await episodeTranslationsService.updateOne(t.id, t)
+            await upsertLS(oldKnex, t.legacy_title_id, oldLang, t.title)
+            await upsertLS(oldKnex, t.legacy_description_id, oldLang, t.description)
+            await upsertLS(oldKnex, t.legacy_extra_description_id, oldLang, t.extra_description)
+            console.log("done adding norwegian")
+        }
+        
+        let legacyEpisode = await oldKnex<EpisodeEntity>("episode").insert(patch).returning("*")
+        console.log(legacyEpisode)
+        await c.database("episodes").update({legacy_id: legacyEpisode[0].Id}).where("id", e.id)
+
+        console.log("insert", patch)
+	});
+
+
+
+	action('items.create', async (m, c) => {
+		console.log('Item created!');
+		console.log(m);
+        if (m.collection != "shows") {
+            return
+        }
+        // get legacy id
+		const showsService = new ItemsService<episodes.components["schemas"]["ItemsShows"]>("shows", {
+			knex: c.database as any,
+			schema: c.schema,
+		});
+        const showTranslationsService = new ItemsService<episodes.components["schemas"]["ItemsShowsTranslations"]>("shows_translations", {
+			knex: c.database as any,
+			schema: c.schema,
+		});
+        let e = await showsService.readOne(Number(m.key), { fields: ['*.*.*'] })
+        let image = e.image_file_id as episodes.components["schemas"]["Files"]
+        console.log("directus", e)
+
+        // update it in original        
+        let patch: Partial<SeriesEntity> = {
+            IsCategory: e.type === "event" ? 1 : 0,
+            Published: e.publish_date as unknown as Date,
+            AvailableTo: e.available_to as unknown as Date,
+            AvailableFrom: e.available_from as unknown as Date,
+            Status: getStatusFromNew(e.status),
+            LastUpdate: e.date_created as unknown as Date
+        }
+
+        if (image != null) {
+            patch.Image = "https://brunstadtv.imgix.net/"+image.filename_disk
+        }
+
+        if (e.status == "published") {
+            patch.Status = 1
+        } else {
+            patch.Status = 0
+        }
+
+        let languages = await oldKnex<LanguageEntity>("language").select("*")
+        
+        for (let t of e.translations) {
+            t = t as episodes.components["schemas"]["ItemsEpisodesTranslations"]
+            let lang = (t.languages_code as episodes.components["schemas"]["ItemsLanguages"])    
+            if (lang.code != "no") {
+                // We want original to be source of truth for translations for now
+                continue
+            }
+            console.log("adding norwegian")
+            console.log("t:", t)
+            let oldLang = languages.find(l => l.CultureCode == lang.code)
+            patch.TitleId = await createLocalizable(oldKnex)
+            patch.DescriptionId = await createLocalizable(oldKnex)
+            console.log(patch)
+            //let ids = await ensureLocalizablesExist(oldKnex, 'episode', patch, {'title': 'legacy_title_id', 'description': 'legacy_description_id', 'long_description': 'legacy_extra_description_id'})
+            
+            t = {
+                ...t,
+                legacy_title_id: patch.TitleId,
+                legacy_description_id: patch.DescriptionId,
+            }
+            await showTranslationsService.updateOne(t.id, t)
+            await upsertLS(oldKnex, t.legacy_title_id, oldLang, t.title)
+            await upsertLS(oldKnex, t.legacy_description_id, oldLang, t.description)
+            console.log("done adding norwegian")
+        }
+        
+        let legacyShow = await oldKnex<SeriesEntity>("series").insert(patch).returning("*")
+        console.log(legacyShow)
+        await c.database("shows").update({legacy_id: legacyShow[0].Id}).where("id", e.id)
+
+        console.log("insert", patch)
 	});
 });
--- cms/src/hooks/sync-to-legacy/utils.ts
+++ cms/src/hooks/sync-to-legacy/utils.ts
@@ -1,4 +1,8 @@
 
+import { LanguageEntity, LocalizedStringEntity } from '@/Database'
+import episodes from '../../btv'
+import * as knex from 'knex'
+import { ItemsService } from 'directus'
 export function getStatusFromOld(status: number): string {
     switch (status) {
         case 0: return "draft"
@@ -11,5 +15,53 @@ export function getStatusFromNew(status: string): number {
     switch (status) {
         case "published": return 1
         default: return 0
+    }
+}
+
+export async function upsertLS(oldKnex: knex.Knex<any, unknown[]>, parentId: number, lang: LanguageEntity, value: string) {
+    console.log(value)
+    let c = await oldKnex<LocalizedStringEntity>("localizedstring")
+    .innerJoin("language", "localizedstring.languageid", "language.id")
+    .where("ParentId", parentId)
+    .andWhere("language.culturecode", lang.CultureCode)
+    .update({
+        Value: value
+    }).returning("*")
+    if (c.length === 0 && value != null) {
+        console.log({
+            ParentId: parentId,
+            LanguageId: lang.Id,
+            Value: value
+        })
+        let inserted = await oldKnex<LocalizedStringEntity>("localizedstring")
+        .insert({
+            ParentId: parentId,
+            LanguageId: lang.Id,
+            Value: value
+        }).returning("*")
+        console.log("inserted", inserted)
+    }
+}
+/* 
+
+export async function ensureLocalizablesExist(oldKnex: knex.Knex<any, unknown[]>, table: string, e: any, fields: {[oldColumn: string]: string}): Promise<{[idColumn: string]: number}> {
+    let ids = {}
+    for (let key in fields) {
+        if (e[key] == null) { // if entity doesnt have a localizablestring for this column
+            ids[fields[key]] = await createLocalizable(oldKnex, table, e, fields[key]) // Create one
+        }
+    }
+    return ids
+} */
+
+export async function createLocalizable(oldKnex: knex.Knex<any, unknown[]>): Promise<number> {
+    let result = await oldKnex<LocalizedStringEntity>("localizablestring").insert({}).returning<[{id: number}]>("id")
+    return result[0].id
+}
+
+export function objectPatch(e: any, fields: {[field: string]: any}) {
+    return {
+        ...e,
+        ...fields
     }
 }
