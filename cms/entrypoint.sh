37dbd694309b68b81e46e4eeb0a291c74113385b
Matjaz Debelak <matjaz@debelak.org>
2022-05-25 14:38:56 +0200

    Fix sqlproxy path

--- cms/entrypoint.sh
+++ cms/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./cloud_sql_proxy -instances brunstadtv:europe-west4:btv-sql-prod=tcp:localhost:1433 &
+/cloud_sql_proxy -instances brunstadtv:europe-west4:btv-sql-prod=tcp:localhost:1433 &
 npx directus bootstrap
 make schema-update
 npx directus start
