ee10bd594f37b825c409d2de7b70a8bf7fbe1cc1
Matjaz Debelak <matjaz@debelak.org>
2022-06-08 15:38:19 +0200

    Base for api Process

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/bcc-code/brunstadtv/backend/graph"
+	"github.com/bcc-code/brunstadtv/backend/graph/generated"
+	"github.com/bcc-code/brunstadtv/backend/sqlc"
+	"github.com/bcc-code/brunstadtv/backend/utils"
+	"github.com/bcc-code/mediabank-bridge/log"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
+	"go.opentelemetry.io/otel"
+)
+
+// Defining the Graphql handler
+func graphqlHandler(queries *sqlc.Queries) gin.HandlerFunc {
+
+	resolver := graph.Resolver{
+		Queries: queries,
+	}
+
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	log.ConfigureGlobalLogger(zerolog.DebugLevel)
+	log.L.Debug().Msg("Seting up tracing!")
+
+	// Here you can get a tracedHttpClient if useful anywhere
+	utils.MustSetupTracing()
+	ctx, span := otel.Tracer("api/core").Start(ctx, "init")
+
+	config := getEnvConfig()
+	db, err := sql.Open("postgres", config.DB.ConnectionString)
+	if err != nil {
+		log.L.Panic().Err(err).Msg("Unable to connect to DB")
+		return
+	}
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.L.Panic().Err(err).Msg("Ping failed")
+		return
+	}
+	queries := sqlc.New(db)
+
+	log.L.Debug().Msg("Set up HTTP server")
+	r := gin.Default()
+
+	r.POST("/query", graphqlHandler(queries))
+
+	// TODO: Should we have this in non-local envs?
+	// What about auth?
+	r.GET("/", playgroundHandler())
+
+	log.L.Debug().Msgf("connect to http://localhost:%s/ for GraphQL playground", config.Port)
+
+	span.End()
+	r.Run(":" + config.Port)
+}
