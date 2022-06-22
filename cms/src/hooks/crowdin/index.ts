64274510077387998a695ba9765c38caac25b955
Fredrik Vedvik <fredrik@vedvik.tech>
2022-06-22 12:50:53 +0200

    cleanup

--- cms/src/hooks/crowdin/index.ts
+++ cms/src/hooks/crowdin/index.ts
@@ -1,6 +1,5 @@
-import {SourceStrings } from "@crowdin/crowdin-api-client";
-import {enabled, getConfig, getCredentials} from "./config";
-import {getTranslationsFromEvent} from "./source";
+import {enabled} from "./config";
+import {updateOrSetTranslationAsync} from "./source";
 
 export type Model = "show" | "season" | "episode";
 export type Collection = "shows_translations" | "seasons_translations" | "episodes_translations";
@@ -16,58 +15,6 @@ export default ({_, action}) => {
     if (!enabled()) 
         return
     
-    action('items.create', async (input: Event<any>) => {
-        if (input.collection.endsWith("_translations")) {
-            console.log(JSON.stringify(input))
-            const config = getConfig();
-            const {model, id, values} = getTranslationsFromEvent(input)
-            if (!model)
-                return;
-
-            const stringApi = new SourceStrings(getCredentials())
-            for (const [field, value] of Object.entries(values)) {
-                const identifier = `${model}-${id}-` + field;
-                await stringApi.addString(config.projectId, {
-                    fileId: config.contentFileId,
-                    text: value,
-                    identifier: identifier,
-                })
-            }
-        }
-    })
-
-    action('items.update', async (input: Event<any>) => {
-        if (input.collection.endsWith("_translations")) {
-            console.log(JSON.stringify(input))
-            const config = getConfig();
-            const {model, id, values} = getTranslationsFromEvent(input)
-
-            if (!model)
-                return;
-
-            const stringApi = new SourceStrings(getCredentials())
-            const strings = await stringApi.listProjectStrings(config.projectId)
-
-            for (const [field, value] of Object.entries(values)) {
-                const identifier = `${model}-${id}-` + field;
-                const existingString = strings.data.find(s => s.data.identifier === identifier)
-
-                if (existingString) {
-                    await stringApi.editString(config.projectId, existingString.data.id, [
-                        {
-                            op: "replace",
-                            path: "/text",
-                            value: value
-                        }
-                    ])
-                } else {
-                    await stringApi.addString(config.projectId, {
-                        fileId: config.contentFileId,
-                        text: value,
-                        identifier: `${model}-${id}-` + field,
-                    })
-                }
-            }
-        }
-    })
+    action('items.create', updateOrSetTranslationAsync)
+    action('items.update', updateOrSetTranslationAsync)
 }
