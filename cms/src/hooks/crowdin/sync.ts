ebf635c5f5b2bf7a35994780a37419f4c838f018
Fredrik Vedvik <fredrik@vedvik.tech>
2022-06-24 09:18:42 +0200

    use both connection string or address/username

--- cms/src/hooks/crowdin/sync.ts
+++ cms/src/hooks/crowdin/sync.ts
@@ -18,6 +18,11 @@ type Translation = {
 const db = knex({
     client: "pg",
     connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         connectionString: process.env.DB_CONNECTION_STRING,
     }
 })
