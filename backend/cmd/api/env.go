70166bd5a04453c663dbae3d26640498ddc1346c
Fredrik Vedvik <fredrik@vedvik.tech>
2022-07-04 11:13:58 +0200

    unused environment variables

--- backend/cmd/api/env.go
+++ backend/cmd/api/env.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/bcc-code/brunstadtv/backend/auth0"
@@ -19,23 +18,11 @@ type algolia struct {
 	SearchOnlyApiKey string
 }
 
-type directusConfig struct {
-	Token string
-	Host  string
-}
-
-type crowdinConfig struct {
-	Token      string
-	ProjectIDs []int
-}
-
 type envConfig struct {
 	DB        postgres
 	Algolia   algolia
 	Port      string
 	JWTConfig auth0.JWTConfig
-	Directus  directusConfig
-	Crowdin   crowdinConfig
 }
 
 func getEnvConfig() envConfig {
@@ -59,18 +46,6 @@ func getEnvConfig() envConfig {
 			AppId:            os.Getenv("ALGOLIA_APP_ID"),
 			ApiKey:           os.Getenv("ALGOLIA_API_KEY"),
 			SearchOnlyApiKey: os.Getenv("ALGOLIA_SEARCH_ONLY_API_KEY"),
-		},
-		Directus: directusConfig{
-			Token: os.Getenv("DU_TOKEN"),
-			Host:  os.Getenv("DU_HOST"),
-		},
-		Crowdin: crowdinConfig{
-			Token: os.Getenv("CROWDIN_TOKEN"),
-			ProjectIDs: lo.Map(strings.Split(os.Getenv("CROWDIN_PROJECT_IDS"), ","),
-				func(s string, _ int) int {
-					r, _ := strconv.ParseInt(s, 10, 64)
-					return int(r)
-				}),
 		},
 	}
 }
