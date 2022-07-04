439dc7fd6b09d6dd17fcc74814401a92c6a48f0a
Matjaz Debelak <matjaz@debelak.org>
2022-07-04 12:19:08 +0200

    Typo + Some comments

--- backend/program/episode.go
+++ backend/program/episode.go
@@ -9,6 +9,7 @@ import (
 	"github.com/samber/lo"
 )
 
+// NewBatchLoader returns a configured batch loader for GQL Program
 func NewBatchLoader(queries sqlc.Queries) *dataloader.Loader[int, gqlmodel.Program] {
 	batchLoadPrograms := func(ctx context.Context, keys []int) []*dataloader.Result[gqlmodel.Program] {
 		results := []*dataloader.Result[gqlmodel.Program]{}
@@ -17,7 +18,7 @@ func NewBatchLoader(queries sqlc.Queries) *dataloader.Loader[int, gqlmodel.Progr
 			return int32(key)
 		})
 
-		res, err := queries.GetEpisodesWithTrnaslationsByID(ctx, ids)
+		res, err := queries.GetEpisodesWithTranslationsByID(ctx, ids)
 
 		resMap := map[int]gqlmodel.Program{}
 
@@ -45,6 +46,9 @@ func NewBatchLoader(queries sqlc.Queries) *dataloader.Loader[int, gqlmodel.Progr
 	return dataloader.NewBatchedLoader(batchLoadPrograms)
 }
 
+// GetByID should be used for retrieving program data
+//
+// It uses the dataloader to efficiently load data from DB or cache (as avalilable)
 func GetByID(ctx context.Context, loader *dataloader.Loader[int, gqlmodel.Program], id int) (gqlmodel.Program, error) {
 	thunk := loader.Load(ctx, id)
 	result, err := thunk()
