b0fc1b69bb4b1c14be7384459dcbd5e459741e7a
Matjaz Debelak <matjaz@debelak.org>
2022-07-06 12:18:43 +0200

    Rename a var

--- backend/common/translations.go
+++ backend/common/translations.go
@@ -6,15 +6,15 @@ import (
 	null_v4 "gopkg.in/guregu/null.v4"
 )
 
-// DefaultLangs is the order of lanuages if none is defined by the user
-var DefaultLangs = []string{"en", "no"}
+// DefaultLanguages is the order of lanuages if none is defined by the user
+var DefaultLanguages = []string{"en", "no"}
 
 // Translations is a map of strings to nullable strings
 type Translations map[string]null_v4.String
 
 // GetTranslation from a translation map based on the fallbacks
 func GetTranslation(langs []string, trans Translations) string {
-	langs = append(langs, DefaultLangs...) // We force the DefaultLangs as the last languages regardless if they have been specified before already
+	langs = append(langs, DefaultLanguages...) // We force the DefaultLangs as the last languages regardless if they have been specified before already
 	for _, l := range langs {
 		if val, ok := trans[l]; ok && val.Valid {
 			return val.String
