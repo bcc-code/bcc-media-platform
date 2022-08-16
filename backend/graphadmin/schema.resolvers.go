7440f253a20843f2ab12e7bea8a49d0b4dc37679
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-16 10:32:44 +0200

    some cleanup

--- backend/graphadmin/schema.resolvers.go
+++ backend/graphadmin/schema.resolvers.go
@@ -6,67 +6,24 @@ package gqladmin
 import (
 	"context"
 	"encoding/json"
-	"strconv"
-
 	"github.com/bcc-code/brunstadtv/backend/common"
 	gqladmingenerated "github.com/bcc-code/brunstadtv/backend/graphadmin/generated"
 	gqladminmodel "github.com/bcc-code/brunstadtv/backend/graphadmin/model"
-	collection2 "github.com/bcc-code/brunstadtv/backend/items/collection"
-	"github.com/bcc-code/brunstadtv/backend/user"
-	"github.com/bcc-code/brunstadtv/backend/utils"
-	"github.com/samber/lo"
 )
 
 // Collection is the resolver for the collection field.
 func (r *previewResolver) Collection(ctx context.Context, obj *gqladminmodel.Preview, collection string, filter string) (*gqladminmodel.PreviewCollection, error) {
-	var f common.Filter
 	ctx = context.WithValue(ctx, "preview", true)
+
+	var f common.Filter
+
 	_ = json.Unmarshal([]byte(filter), &f)
-	ids, err := collection2.GetItemIDsForFilter(ctx, r.DB, collection, f)
+
+	items, err := r.getItemsForFilter(ctx, collection, f)
 	if err != nil {
 		return nil, err
 	}
-	var items []*gqladminmodel.CollectionItem
-	ginCtx, err := utils.GinCtx(ctx)
-	if err != nil {
-		return nil, err
-	}
-	languages := user.GetLanguagesFromCtx(ginCtx)
-	switch collection {
-	case "shows":
-		shows, err := common.GetManyFromLoader(ctx, r.Loaders.ShowLoader, ids)
-		items = lo.Map(shows, func(s *common.Show, _ int) *gqladminmodel.CollectionItem {
-			return &gqladminmodel.CollectionItem{
-				ID:    strconv.Itoa(s.ID),
-				Title: s.Title.Get(languages),
-			}
-		})
-		if err != nil {
-			return nil, err
-		}
-	case "seasons":
-		shows, err := common.GetManyFromLoader(ctx, r.Loaders.SeasonLoader, ids)
-		items = lo.Map(shows, func(s *common.Season, _ int) *gqladminmodel.CollectionItem {
-			return &gqladminmodel.CollectionItem{
-				ID:    strconv.Itoa(s.ID),
-				Title: s.Title.Get(languages),
-			}
-		})
-		if err != nil {
-			return nil, err
-		}
-	case "episodes":
-		shows, err := common.GetManyFromLoader(ctx, r.Loaders.EpisodeLoader, ids)
-		items = lo.Map(shows, func(s *common.Episode, _ int) *gqladminmodel.CollectionItem {
-			return &gqladminmodel.CollectionItem{
-				ID:    strconv.Itoa(s.ID),
-				Title: s.Title.Get(languages),
-			}
-		})
-		if err != nil {
-			return nil, err
-		}
-	}
+
 	return &gqladminmodel.PreviewCollection{
 		Items: items,
 	}, nil
