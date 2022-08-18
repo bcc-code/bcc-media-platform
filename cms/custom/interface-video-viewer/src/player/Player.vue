8ae865919c6cd0ab79ccd6621970e9b0f8388b74
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-18 09:40:50 +0200

    strims

---
+++ cms/custom/interface-video-viewer/src/player/Player.vue
@@ -0,0 +1,43 @@
+<template>
+    <video id="video-player"></video>
+</template>
+<script lang="ts" setup>
+import Hls from "hls.js"
+import Dash from "dashjs"
+import { onMounted, watch } from "vue";
+
+const props = defineProps<{
+    asset: {
+        url: string;
+        type: string;
+    }
+}>()
+
+const load = () => {
+    const video = document.getElementById("video-player") as HTMLVideoElement;
+    if (!video) {
+        return
+    }
+    switch (props.asset.type) {
+        case "hls-cmaf":
+        case "hls-ts":
+            if (Hls.isSupported()) {
+                const hls = new Hls()
+                hls.loadSource(props.asset.url)
+                hls.attachMedia(video)
+                // Autoplay on load
+                hls.on(Hls.Events.MANIFEST_LOADED, () => {
+                    video.play()
+                })
+            }
+        case "dash":
+            if (Dash.supportsMediaSource()) {
+                const player = Dash.MediaPlayer().create();
+                player.initialize(video, props.asset.url, true)
+            }
+    }
+}
+
+onMounted(load)
+watch(() => props.asset, load)
+</script>
--- cms/custom/interface-video-viewer/src/viewer/Viewer.vue
+++ cms/custom/interface-video-viewer/src/viewer/Viewer.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
-        <media-controller class="video-controller">
-            <video id="viewer-video" slot="media">
-            </video>
+        <media-controller class="video-controller" v-if="asset">
+            <Player :asset="asset" slot="media"></Player>
             <media-control-bar>
                 <media-play-button></media-play-button>
                 <media-mute-button></media-mute-button>
@@ -21,8 +20,8 @@
     </div>
 </template>
 <script lang="ts" setup>
-import { ref, computed } from "vue"
-import Hls from "hls.js"
+import { ref } from "vue"
+import Player from "../player/Player.vue";
 
 const props = defineProps<{
     factory: () => Promise<{
@@ -31,42 +30,17 @@ const props = defineProps<{
     }>
 }>()
 
-const videoSrc = ref("")
+const asset = ref(null as {type: string, url: string} | null)
+
 const error = ref(null as string | null)
 const loading = ref(false)
-const type = ref("")
-
-const isHLS = computed(() => {
-    return type.value.includes("hls")
-})
 
 const load = async () => {
+    asset.value = null
     error.value = null
     loading.value = true
     try {
-        const config = await props.factory();
-        type.value = config.type;
-        videoSrc.value = config.url;
-        if (!isHLS.value) {
-            return
-        }
-        const el = document.getElementById("viewer-video") as HTMLVideoElement
-        if (!el) {
-            return
-        }
-        
-        if (el.canPlayType("application/vnd.apple.mpegurl")) {
-            el.src = config.url;
-            console.log("running")
-        } else {
-            const hls = new Hls();
-            hls.loadSource(config.url)
-            hls.attachMedia(el)
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                el.play();
-            })
-        }
-
+        asset.value = await props.factory();
     } catch {
         error.value = "Couldn't retrieve from API"
     } finally {
