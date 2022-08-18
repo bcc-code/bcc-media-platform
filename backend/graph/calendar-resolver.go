fdc216fcf5991222c93c0240b45b390d22b1d0e3
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-18 15:00:35 +0200

    active days

--- backend/graph/calendar-resolver.go
+++ backend/graph/calendar-resolver.go
@@ -22,16 +22,8 @@ func getForPeriod[k comparable, t any](ctx context.Context, loader *dataloader.L
 	return items, nil
 }
 
-func (r *calendarResolver) periodResolver(ctx context.Context, from time.Time, to time.Time) (*gqlmodel.CalendarPeriod, error) {
-	events, err := getForPeriod(ctx, r.Loaders.EventLoader, r.Queries.GetEventsForPeriod, from, to)
-	if err != nil {
-		return nil, err
-	}
-	entries, err := getForPeriod(ctx, r.Loaders.CalendarEntryLoader, r.Queries.GetCalendarEntriesForPeriod, from, to)
-	if err != nil {
-		return nil, err
-	}
-	zone, offset := from.Zone()
+func getActiveDays(entries []*common.CalendarEntry, sourceTime time.Time) []string {
+	zone, offset := sourceTime.Zone()
 	location := time.FixedZone(zone, offset)
 
 	var days []string
@@ -43,11 +35,24 @@ func (r *calendarResolver) periodResolver(ctx context.Context, from time.Time, t
 		}
 	}
 
+	return lo.Map(days, func(date string, _ int) string {
+		t, _ := time.ParseInLocation("2006-01-02", date, location)
+		return t.Format(time.RFC3339)
+	})
+}
+
+func (r *calendarResolver) periodResolver(ctx context.Context, from time.Time, to time.Time) (*gqlmodel.CalendarPeriod, error) {
+	events, err := getForPeriod(ctx, r.Loaders.EventLoader, r.Queries.GetEventsForPeriod, from, to)
+	if err != nil {
+		return nil, err
+	}
+	entries, err := getForPeriod(ctx, r.Loaders.CalendarEntryLoader, r.Queries.GetCalendarEntriesForPeriod, from, to)
+	if err != nil {
+		return nil, err
+	}
+
 	return &gqlmodel.CalendarPeriod{
-		Events: utils.MapWithCtx(ctx, events, gqlmodel.EventFrom),
-		ActiveDays: lo.Map(days, func(date string, _ int) string {
-			t, _ := time.ParseInLocation("2006-01-02", date, location)
-			return t.Format(time.RFC3339)
-		}),
+		Events:     utils.MapWithCtx(ctx, events, gqlmodel.EventFrom),
+		ActiveDays: getActiveDays(entries, from),
 	}, nil
 }
--- backend/sqlc/calendar.sql.go
+++ backend/sqlc/calendar.sql.go
@@ -153,14 +153,8 @@ func (q *Queries) getCalendarEntriesForEvents(ctx context.Context, dollar_1 []in
 }
 
 const getCalendarEntryIDsForPeriod = `-- name: getCalendarEntryIDsForPeriod :many
-WITH t AS (SELECT ts.calendarentries_id,
-                  json_object_agg(ts.languages_code, ts.title)       AS title,
-                  json_object_agg(ts.languages_code, ts.description) AS description
-           FROM calendarentries_translations ts
-           GROUP BY ts.calendarentries_id)
 SELECT e.id
 FROM calendarentries e
-         LEFT JOIN t ON e.id = t.calendarentries_id
 WHERE e.status = 'published'
   AND ((e.start >= $1::timestamptz AND e.start <= $2::timestamptz) OR
        (e.end >= $1::timestamptz AND e.end <= $2::timestamptz))
@@ -196,13 +190,8 @@ func (q *Queries) getCalendarEntryIDsForPeriod(ctx context.Context, arg getCalen
 }
 
 const getEventIDsForPeriod = `-- name: getEventIDsForPeriod :many
-WITH t AS (SELECT ts.events_id,
-                  json_object_agg(ts.languages_code, ts.title)       AS title
-           FROM events_translations ts
-           GROUP BY ts.events_id)
 SELECT e.id
 FROM events e
-         LEFT JOIN t ON e.id = t.events_id
 WHERE e.status = 'published'
   AND ((e.start >= $1::timestamptz AND e.start <= $2::timestamptz) OR
        (e.end >= $1::timestamptz AND e.end <= $2::timestamptz))
