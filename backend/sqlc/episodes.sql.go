ef0d2f4852ed3a5f338758b5dc854e48b98c1fbf
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-25 13:37:08 +0200

    actual permissions fix

--- backend/sqlc/episodes.sql.go
+++ backend/sqlc/episodes.sql.go
@@ -153,8 +153,8 @@ WITH er AS (SELECT e.id,
                             ARRAY []::character varying[])                                AS roles_earlyaccess
             FROM episodes e),
      ea AS (SELECT e.id,
-                   (e.status::text = 'published'::text AND se.status::text = 'published'::text AND
-                    s.status::text = 'published'::text)::bool                   AS published,
+                   e.status::text = 'published'::text AND (e.season_id IS NULL OR (se.status::text = 'published'::text AND
+                                                                                   s.status::text = 'published'::text))                  AS published,
                    COALESCE(GREATEST(e.available_from, se.available_from, s.available_from),
                             '1800-01-01 00:00:00'::timestamp without time zone) AS available_from,
                    COALESCE(LEAST(e.available_to, se.available_to, s.available_to),
@@ -163,7 +163,7 @@ WITH er AS (SELECT e.id,
                      LEFT JOIN seasons se ON e.season_id = se.id
                      LEFT JOIN shows s ON se.show_id = s.id)
 SELECT e.id,
-       COALESCE(access.published::bool, false)::bool AS published,
+       access.published::bool AS published,
        access.available_from::timestamp              AS available_from,
        access.available_to::timestamp                AS available_to,
        roles.roles::varchar[]                        AS usergroups,
