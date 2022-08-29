2570dfea223d1894b7f658edbc2aa1c96038243e
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-29 11:22:58 +0200

    router

---
+++ web/src/router/routes.ts
@@ -0,0 +1,5 @@
+import { RouteRecordRaw } from "vue-router";
+
+export default [
+    
+] as RouteRecordRaw[]
--- web/src/utils/theme.ts
+++ web/src/utils/theme.ts
@@ -1,6 +1,6 @@
 export type Theme = "dark" | "light"
 
-export const reload = () => {
+export const current = () => {
     const theme = localStorage.getItem("theme") as Theme | null
     let dark = theme === "dark"
     if (!theme) {
@@ -9,5 +9,34 @@ export const reload = () => {
         }
     }
 
-    
+    return dark ? "dark" : "light" as Theme
+}
+
+export const reload = () => {
+    const theme = current()
+
+    const classList = document.documentElement.classList;
+
+    if (theme === "dark") {
+        if (!classList.contains("dark")) {
+            classList.add("dark")
+        }
+    } else {
+        if (classList.contains("dark")) {
+            classList.remove("dark")
+        }
+    }
+}
+
+export const set = (theme: Theme) => {
+    localStorage.setItem("theme", theme)
+    reload()
+}
+
+export const toggle = () => {
+    if (current() === "dark") {
+        set("light")
+    } else {
+        set("dark")
+    }
 }
