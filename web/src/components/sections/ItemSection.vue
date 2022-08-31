cbd5e3bfa566763df71b82e69a0d64c6a3b9aad8
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-31 11:34:39 +0200

    page

--- web/src/components/sections/ItemSection.vue
+++ web/src/components/sections/ItemSection.vue
@@ -7,6 +7,7 @@
                     class="m-1 w-64 top-0 h-full inline-block cursor-pointer shadow hover:shadow-lg hover:shadow-inner"
                     v-for="item in section.items.items"
                     :item="item"
+                    @click="view(item)"
                 ></SectionItem>
             </div>
         </div>
@@ -14,10 +15,58 @@
     </div>
 </template>
 <script lang="ts" setup>
-import { Section } from "./types"
+import { Section, SectionItem as TSectionItem } from "./types"
 import SectionItem from "./SectionItem.vue"
+import { useRouter } from "vue-router";
 
 defineProps<{
     section: Section
 }>()
+
+const router = useRouter()
+
+const view = (item: TSectionItem) => {
+    switch (item.__typename) {
+        case "EpisodeItem":
+            router.push({
+                name: "episode-page",
+                params: {
+                    episodeId: item.id,
+                }
+            })
+            break;
+            case "SeasonItem":
+            router.push({
+                name: "season-page",
+                params: {
+                    seasonId: item.id,
+                }
+            })
+            break;
+            case "ShowItem":
+            router.push({
+                name: "show-page",
+                params: {
+                    showId: item.id,
+                }
+            })
+            break;
+            case "PageItem":
+            router.push({
+                name: "page",
+                params: {
+                    pageId: item.page.code,
+                }
+            })
+            break;
+            case "URLItem":
+            // router.push({
+            //     name: "episode-page",
+            //     params: {
+            //         episodeId: item.id,
+            //     }
+            // })
+            break;
+    }
+}
 </script>
