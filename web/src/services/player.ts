14863edb6460aebdb73f6ad130c8441ec56549d7
Fredrik Vedvik <fredrik@vedvik.tech>
2022-09-14 11:58:42 +0200

    player style

--- web/src/services/player.ts
+++ web/src/services/player.ts
@@ -1,6 +1,6 @@
 import config from "@/config"
 import { PlayerFactory } from "bccm-video-player"
-import "@/../node_modules/bccm-video-player/build/style.css"
+import "bccm-video-player/css"
 import Auth from "./auth"
 
 export default new PlayerFactory({
