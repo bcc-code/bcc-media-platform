69913b0c9f144b98b02ddf0bc2ce7a62dfe73250
Fredrik Vedvik <fredrik@vedvik.tech>
2022-09-28 10:38:25 +0200

    .

--- web/src/pages/Page.vue
+++ web/src/pages/Page.vue
@@ -7,7 +7,8 @@
             >
             </ItemSection>
         </div>
-        <div v-else-if="!fetching">Uh oh. Missing content</div>
+        <div v-if="fetching">FETCHING</div>
+        <div v-else-if="!data?.page.sections.items.length">Uh oh. Missing content</div>
         <div v-if="error">{{ error.message }}</div>
     </section>
 </template>
