e1cc29029e774193265969e4a5708dfa8f50f26e
Fredrik Vedvik <fredrik@vedvik.tech>
2022-10-10 15:01:05 +0200

    padding

--- web/src/components/search/SearchItem.vue
+++ web/src/components/search/SearchItem.vue
@@ -1,13 +1,14 @@
 <template>
     <div
-        class="flex aspect-video bg-cover bg-center bg-no-repeat rounded rounded-2xl"
+        class="flex relative aspect-video bg-cover bg-center bg-no-repeat rounded rounded-2xl"
         :style="{
             'background-image':
                 'linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(\'' +
                 item.image +
                 '\')',
         }"
-    >
+    >  
+        <div v-if="adminOn" class="absolute text-primary right-0 bg-black p-2 rounded cursor-pointer m-2" @click="open">EDIT</div> 
         <h1 class="text-lg p-2 text-primary font-semibold mt-auto">
             {{ item.title }}
         </h1>
@@ -16,7 +17,26 @@
 <script lang="ts" setup>
 import { SearchQuery } from "@/graph/generated"
 
-defineProps<{
+const props = defineProps<{
     item: SearchQuery["search"]["result"][0]
 }>()
+
+const adminOn = localStorage.getItem("admin") === "true"
+
+const open = () => {
+    let collection = ""
+    switch (props.item.__typename) {
+        case "EpisodeSearchItem":
+            collection = "episodes"
+            break;
+            case "SeasonSearchItem":
+                collection = "seasons"
+                break;
+                case "ShowSearchItem":
+                    collection = "shows"
+                    break;
+    }
+
+    window.open("https://admin.brunstad.tv/admin/content/"+ collection + "/" + props.item.id)
+}
 </script>
