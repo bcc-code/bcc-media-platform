164db4088af430cceb373681ea6b67aeda3fe1e6
Matjaz Debelak <matjaz.debelak@bcc.no>
2022-10-12 16:44:04 +0200

    Remove owner alterations from migration

--- migrations/00024_add_awsarn_field.sql
+++ migrations/00024_add_awsarn_field.sql
@@ -12,24 +12,6 @@ COMMENT ON COLUMN "public"."assets"."aws_arn"  IS NULL;
 
 --- END ALTER TABLE "public"."assets" ---
 
---- BEGIN ALTER TABLE "public"."shows_tags" ---
-
-ALTER TABLE IF EXISTS "public"."shows_tags" OWNER TO builder;
-
---- END ALTER TABLE "public"."shows_tags" ---
-
---- BEGIN ALTER TABLE "public"."sections_links" ---
-
-ALTER TABLE IF EXISTS "public"."sections_links" OWNER TO builder;
-
---- END ALTER TABLE "public"."sections_links" ---
-
---- BEGIN ALTER TABLE "public"."images" ---
-
-ALTER TABLE IF EXISTS "public"."images" OWNER TO builder;
-
---- END ALTER TABLE "public"."images" ---
-
 --- BEGIN SYNCHRONIZE TABLE "public"."directus_fields" RECORDS ---
 
 INSERT INTO "public"."directus_fields" ("id", "collection", "field", "special", "interface", "options", "display", "display_options", "readonly", "hidden", "sort", "width", "translations", "note", "conditions", "required", "group", "validation", "validation_message")  VALUES (614, 'assets', 'aws_arn', NULL, 'input', NULL, NULL, NULL, true, false, NULL, 'full', NULL, NULL, NULL, false, NULL, NULL, NULL);
@@ -46,24 +28,6 @@ INSERT INTO "public"."directus_fields" ("id", "collection", "field", "special", 
 ALTER TABLE IF EXISTS "public"."assets" DROP COLUMN IF EXISTS "aws_arn" CASCADE; --WARN: Drop column can occure in data loss!
 
 --- END ALTER TABLE "public"."assets" ---
-
---- BEGIN ALTER TABLE "public"."shows_tags" ---
-
-ALTER TABLE IF EXISTS "public"."shows_tags" OWNER TO btv;
-
---- END ALTER TABLE "public"."shows_tags" ---
-
---- BEGIN ALTER TABLE "public"."sections_links" ---
-
-ALTER TABLE IF EXISTS "public"."sections_links" OWNER TO btv;
-
---- END ALTER TABLE "public"."sections_links" ---
-
---- BEGIN ALTER TABLE "public"."images" ---
-
-ALTER TABLE IF EXISTS "public"."images" OWNER TO btv;
-
---- END ALTER TABLE "public"."images" ---
 
 --- BEGIN SYNCHRONIZE TABLE "public"."directus_fields" RECORDS ---
 
