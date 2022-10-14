d3679d60abd2bb2bd76778d95b91e45d77603668
Fredrik Vedvik <fredrik@vedvik.tech>
2022-10-14 14:35:59 +0200

    some notification things

--- backend/push/client.go
+++ backend/push/client.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"firebase.google.com/go"
 	"firebase.google.com/go/messaging"
-	"github.com/bcc-code/mediabank-bridge/log"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/ansel1/merry/v2"
+	"github.com/bcc-code/brunstadtv/backend/common"
+	"github.com/samber/lo/parallel"
 )
 
 // Service is the struct containing the firebase app and methods for interacting with messaging
@@ -26,33 +27,73 @@ func NewService(ctx context.Context, firebaseProjectID string) (*Service, error)
 	}, nil
 }
 
-// SendPushNotification sends a push notification to the specified tokens
-func (s *Service) SendPushNotification(ctx context.Context, tokens []string) {
+func notificationToPayload(notification common.Notification) map[string]string {
+	var payload = map[string]string{}
+	for lan, val := range notification.Title {
+		if val.Valid {
+			payload["title_"+lan] = val.ValueOrZero()
+		}
+	}
+	for lan, val := range notification.Description {
+		if val.Valid {
+			payload["description_"+lan] = val.ValueOrZero()
+		}
+	}
+	for lan, val := range notification.Image {
+		if val != "" {
+			payload["image_"+lan] = val
+		}
+	}
+	return payload
+}
+
+// SendNotificationToDevices sends a push notification to the specified tokens
+func (s *Service) SendNotificationToDevices(ctx context.Context, deviceTokens []string, notification common.Notification) error {
 	// TODO: actually implement notifications, not this placeholder things
 	client, err := s.app.Messaging(ctx)
 	if err != nil {
-		return
+		return err
 	}
 
 	var messages []*messaging.Message
 
-	for _, t := range tokens {
+	data := notificationToPayload(notification)
+
+	for _, t := range deviceTokens {
 		messages = append(messages, &messaging.Message{
-			Data: nil,
-			Notification: &messaging.Notification{
-				Title: "Notification title",
-				Body:  "This is the content for this notification",
-			},
-			Android:    nil,
-			Webpush:    nil,
-			APNS:       nil,
-			FCMOptions: nil,
-			Token:      t,
-			Topic:      "",
-			Condition:  "",
+			Data:  data,
+			Token: t,
 		})
 	}
 
-	res, err := client.SendAll(ctx, messages)
-	log.L.Debug().Str("result", spew.Sdump(res)).Msg("notification sent result")
+	const maxConcurrent = 200
+
+	var ranges [][]*messaging.Message
+
+	for i := 0; i < len(messages); i += maxConcurrent {
+		var r int
+		if i+maxConcurrent > len(messages) {
+			r = len(messages)
+		} else {
+			r = i + maxConcurrent
+		}
+		ranges = append(ranges, messages[i:r])
+	}
+
+	errors := parallel.Map(ranges, func(r []*messaging.Message, _ int) error {
+		res, err := client.SendAll(ctx, r)
+		//TODO: Implement error checking
+		if err != nil {
+			return err
+		}
+		if res.FailureCount > 0 {
+			return merry.New("Failure-count is not zero")
+		}
+		return nil
+	})
+
+	if len(errors) > 0 {
+		return errors[0]
+	}
+	return nil
 }
