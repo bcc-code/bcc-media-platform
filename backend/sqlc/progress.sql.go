61243e7774f17d5dd170ebdadbbcfcabe9b52721
Fredrik Vedvik <fredrik@vedvik.tech>
2022-11-15 14:26:22 +0100

    .

--- backend/sqlc/progress.sql.go
+++ backend/sqlc/progress.sql.go
@@ -36,7 +36,7 @@ const getEpisodeIDsWithProgress = `-- name: getEpisodeIDsWithProgress :many
 SELECT p.episode_id, p.profile_id
 FROM "users"."progress" p
 WHERE p.profile_id = ANY ($1::uuid[])
-  AND COALESCE((p.progress::float / NULLIF(p.duration, 0)) > 0.8, false) != true
+  AND COALESCE((p.progress::float / COALESCE(NULLIF(p.duration, 0), 1)) > 0.8, false) != true
 ORDER BY p.updated_at DESC
 `
 
