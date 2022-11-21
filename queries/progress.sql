8cfd4e6df0a7b21693a62d7cd1f78aa0d77d1846
Fredrik Vedvik <fredrik@vedvik.tech>
2022-11-21 11:02:11 +0100

    fix progress unique

--- queries/progress.sql
+++ queries/progress.sql
@@ -23,8 +23,19 @@ WHERE p.profile_id = $1
   AND p.episode_id = $2;
 
 -- name: getEpisodeIDsWithProgress :many
+WITH shows AS (SELECT DISTINCT ON (p.show_id, p.profile_id) p.show_id,
+                                                            p.profile_id,
+                                                            p.episode_id,
+                                                            p.updated_at
+               FROM "users"."progress" p
+               WHERE p.show_id IS NOT NULL
+               GROUP BY p.profile_id, p.show_id, p.episode_id
+               ORDER BY p.show_id, p.profile_id, p.updated_at DESC)
 SELECT p.episode_id, p.profile_id
 FROM "users"."progress" p
+         LEFT JOIN shows s ON p.show_id = s.show_id AND p.profile_id = s.profile_id
 WHERE p.profile_id = ANY ($1::uuid[])
-  AND COALESCE((p.progress::float / COALESCE(NULLIF(p.duration, 0), 1)) > 0.8, false) != true
+    AND (s IS NULL
+    OR s.episode_id = p.episode_id)
+    AND COALESCE((p.progress::float / COALESCE(NULLIF(p.duration, 0), 1)) > 0.8, false) != true
 ORDER BY p.updated_at DESC;
