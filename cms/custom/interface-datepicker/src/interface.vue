c2008677fc23c85b19ec47dbc6a5703499364d55
Fredrik Vedvik <fredrik@vedvik.tech>
2022-12-01 11:08:31 +0100

    directus better datepicker

---
+++ cms/custom/interface-datepicker/src/interface.vue
@@ -0,0 +1,44 @@
+<template>
+	<div style="height: 100%">
+		<Datepicker 
+			v-model="date" 
+			style="height: 100%"
+			:text-input="true"
+			format="dd/MM/yyyy HH:mm"
+			:text-input-options="{
+				enterSubmit: true,
+				openMenu: true,
+				format: ['dd/MM/yy HH:mm', 'dd/MM/yyyy HH:mm', 'dd/MM HH:mm', 'dd HH:mm'],
+			}"
+	  />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<{
+	value: string | null
+}>(), {value: null})
+
+const date = computed({
+	get() {
+		return props.value ? props.value : null
+	},
+	set(v) {
+		emit("input", v ? v : null)
+	}
+})
+
+const emit = defineEmits<{
+	(e: "input", value: string | null)
+}>()
+</script>
+<style>
+.dp__input {
+    font-size: 1.25rem;
+    line-height: 3rem;
+}
+</style>
