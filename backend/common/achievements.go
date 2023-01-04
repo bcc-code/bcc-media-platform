e4aa69bcf1d17f8bd16091937e9260e623c50d1f
Fredrik Vedvik <fredrik@vedvik.tech>
2023-01-04 13:54:12 +0100

    get key

--- backend/common/achievements.go
+++ backend/common/achievements.go
@@ -39,8 +39,14 @@ func (a AchievementGroup) GetKey() uuid.UUID {
 	return a.ID
 }
 
+// Achieved struct
 type Achieved struct {
 	ID          uuid.UUID
 	AchievedAt  time.Time
 	ConfirmedAt null.Time
+}
+
+// GetKey returns key for this item
+func (a Achieved) GetKey() uuid.UUID {
+	return a.ID
 }
