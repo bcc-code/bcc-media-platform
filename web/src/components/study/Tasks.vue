cb85caf7617d7f47a3557a4bb3c33824d8b71317
Andreas Gangsø <andreas.gangso@bcc.no>
2023-01-10 10:16:22 +0100

    fix(study): overflow scroll on tasks

--- web/src/components/study/Tasks.vue
+++ web/src/components/study/Tasks.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-full pb-36">
         <div
-            class="inline-flex flex-col space-y-6 items-center justify-start w-full h-full"
+            class="inline-flex flex-col space-y-6 items-center justify-start w-full h-full overflow-scroll"
         >
             <div class="p-4 flex flex-col space-y-0.5 h-14 w-full">
                 <template v-if="tasks.length > 1">
