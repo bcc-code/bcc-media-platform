8357b89aabe4751865184e9a1961c18516f8b108
Fredrik Vedvik <fredrik@vedvik.tech>
2023-01-31 09:37:32 +0100

    tests

--- tests/e2e/tests/episode.test.ts
+++ tests/e2e/tests/episode.test.ts
@@ -5,38 +5,38 @@ import { client } from "lib/directus"
 
 authed(test)
 
-// test("create episode", async (t) => {
-//     const title = "EPISODE"
+test("create episode", async (t) => {
+    const title = "EPISODE"
 
-//     const episode = await client.items("episodes").createOne({
-//         status: "published",
-//         production_date: new Date().toISOString(),
-//         publish_date: new Date().toISOString(),
-//         usergroups: [
-//             {
-//                 usergroups_code: "public",
-//             },
-//         ],
-//         translations: [
-//             {
-//                 languages_code: "no",
-//                 title,
-//             },
-//         ],
-//     })
+    const episode = await client.items("episodes").createOne({
+        status: "published",
+        production_date: new Date().toISOString(),
+        publish_date: new Date().toISOString(),
+        usergroups: [
+            {
+                usergroups_code: "public",
+            },
+        ],
+        translations: [
+            {
+                languages_code: "no",
+                title,
+            },
+        ],
+    })
 
-//     t.truthy(episode, "episode is not null")
+    t.truthy(episode, "episode is not null")
 
-//     const existing = await client.items("episodes").readOne(episode.id ?? 0)
+    const existing = await client.items("episodes").readOne(episode.id ?? 0)
 
-//     t.truthy(existing, "episode exists")
+    t.truthy(existing, "episode exists")
 
-//     const queryString =
-//         "query($episodeId: ID!){ episode(id: $episodeId) { title }}"
+    const queryString =
+        "query($episodeId: ID!){ episode(id: $episodeId) { title }}"
 
-//     const apiResponse = await query(queryString, {
-//         episodeId: episode.id!,
-//     })
+    const apiResponse = await query(queryString, {
+        episodeId: episode.id!,
+    })
 
-//     t.is(title, apiResponse.data.episode.title, "titles not equal")
-// })
+    t.is(title, apiResponse.data.episode.title, "titles not equal")
+})
