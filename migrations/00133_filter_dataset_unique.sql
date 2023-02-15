a8e73690dd01e35366a2892c9a9cee9c6675de39
Fredrik Vedvik <fredrik@vedvik.tech>
2023-02-15 10:22:24 +0100

    grant to api

--- migrations/00133_filter_dataset_unique.sql
+++ migrations/00133_filter_dataset_unique.sql
@@ -86,15 +86,8 @@ UNION
 
 CREATE UNIQUE INDEX index_name ON public.filter_dataset USING btree (uuid);
 
-ALTER MATERIALIZED VIEW IF EXISTS "public"."filter_dataset" OWNER TO btv;
-
-GRANT SELECT ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT INSERT ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT UPDATE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT DELETE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT TRUNCATE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT REFERENCES ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT TRIGGER ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
+GRANT SELECT ON TABLE "public"."filter_dataset" TO api; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
+GRANT ALL ON TABLE "public"."filter_dataset" TO background_worker;
 
 COMMENT ON MATERIALIZED VIEW "public"."filter_dataset"  IS NULL;
 
@@ -182,15 +175,6 @@ UNION
      LEFT JOIN show_availability av ON ((av.id = sh.id)))
      LEFT JOIN sh_tags ON ((sh_tags.id = sh.id)));
 
-ALTER MATERIALIZED VIEW IF EXISTS "public"."filter_dataset" OWNER TO btv;
-
-GRANT SELECT ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT INSERT ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT UPDATE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT DELETE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT TRUNCATE ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT REFERENCES ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-GRANT TRIGGER ON TABLE "public"."filter_dataset" TO btv; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
 GRANT SELECT ON TABLE "public"."filter_dataset" TO api; --WARN: Grant\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
 
 COMMENT ON MATERIALIZED VIEW "public"."filter_dataset"  IS NULL;
