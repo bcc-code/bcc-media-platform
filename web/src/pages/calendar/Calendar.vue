5a1ccb96682de90fe0aef8301fcf568eabe682eb
Fredrik Vedvik <fredrik@vedvik.tech>
2023-04-18 10:25:02 +0200

    prettier run

--- web/src/pages/calendar/Calendar.vue
+++ web/src/pages/calendar/Calendar.vue
@@ -2,9 +2,13 @@
     <section>
         <div class="">
             <div class="max-w-lg mx-auto flex mb-4 stroke-white">
-                <ChevronLeft class="w-16 h-16 cursor-pointer rounded-full hover:bg-gray hover:bg-opacity-10"
-                    @click="incrementMonth(-1)"></ChevronLeft>
-                <p class="w-full text-center text-xl my-auto font-medium uppercase">
+                <ChevronLeft
+                    class="w-16 h-16 cursor-pointer rounded-full hover:bg-gray hover:bg-opacity-10"
+                    @click="incrementMonth(-1)"
+                ></ChevronLeft>
+                <p
+                    class="w-full text-center text-xl my-auto font-medium uppercase"
+                >
                     {{
                         month.toLocaleString([current.code, "en"], {
                             month: "long",
@@ -12,59 +16,74 @@
                     }}
                     {{ month.getFullYear() }}
                 </p>
-                <ChevronRight class="w-16 h-16 cursor-pointer rounded-full hover:bg-gray hover:bg-opacity-10"
-                    @click="incrementMonth(1)"></ChevronRight>
+                <ChevronRight
+                    class="w-16 h-16 cursor-pointer rounded-full hover:bg-gray hover:bg-opacity-10"
+                    @click="incrementMonth(1)"
+                ></ChevronRight>
             </div>
             <div class="p-4 flex flex-col max-w-lg mx-auto">
                 <div class="flex mb-4 w-full">
-                    <div v-for="day in weeks[0]"
-                        class="text-center cursor-pointer hover:bg-gray hover:bg-opacity-10 rounded-full w-full">
+                    <div
+                        v-for="day in weeks[0]"
+                        class="text-center cursor-pointer hover:bg-gray hover:bg-opacity-10 rounded-full w-full"
+                    >
                         <span class="align-middle text-gray">
                             {{ day.toDateString().substring(0, 1) }}
                         </span>
                     </div>
                 </div>
                 <div v-for="week in weeksComputed" class="flex mx-auto w-full">
-                    <div v-for="day in week"
+                    <div
+                        v-for="day in week"
                         class="relative text-center cursor-pointer hover:bg-gray hover:bg-opacity-10 w-full align-middle"
-                        @click="setDay(day.date)">
+                        @click="setDay(day.date)"
+                    >
                         <div class="w-full absolute top-0 z-0">
-                            <div class="bg-opacity-20 h-8 z-0" :class="[
-                                day.in
-                                    ? 'bg-gray'
-                                    : day.start
+                            <div
+                                class="bg-opacity-20 h-8 z-0"
+                                :class="[
+                                    day.in
+                                        ? 'bg-gray'
+                                        : day.start
                                         ? 'bg-gray ml-4 rounded-l-full'
                                         : day.end
-                                            ? 'bg-gray mr-4 rounded-r-full'
-                                            : '',
-                            ]"></div>
+                                        ? 'bg-gray mr-4 rounded-r-full'
+                                        : '',
+                                ]"
+                            ></div>
                         </div>
-                        <div class="h-8 aspect-square mx-auto rounded-full font-bold z-10" :class="[
-                            day.date.getTime() === now.getTime()
-                                ? 'text-red'
-                                : day.date.getMonth() === month.getMonth()
+                        <div
+                            class="h-8 aspect-square mx-auto rounded-full font-bold z-10"
+                            :class="[
+                                day.date.getTime() === now.getTime()
+                                    ? 'text-red'
+                                    : day.date.getMonth() === month.getMonth()
                                     ? ''
                                     : 'text-gray',
-                            day.date.getTime() === selected.getTime()
-                                ? 'outline outline-white outline-2 bg-gray bg-opacity-20'
-                                : 'outline-none',
-                        ]">
+                                day.date.getTime() === selected.getTime()
+                                    ? 'outline outline-white outline-2 bg-gray bg-opacity-20'
+                                    : 'outline-none',
+                            ]"
+                        >
                             <span class="align-middle leading-8">{{
                                 day.date.getDate()
                             }}</span>
                         </div>
                         <div class="flex p-2">
-                            <div class="h-2 w-2 rounded-full mx-auto" :class="[
-                                data?.calendar?.period.activeDays.some(
-                                    (d) =>
-                                        new Date(d).getMonth() ===
-                                        day.date.getMonth() &&
-                                        new Date(d).getDate() ===
-                                        day.date.getDate()
-                                )
-                                    ? 'bg-gray'
-                                    : 'bg-none',
-                            ]"></div>
+                            <div
+                                class="h-2 w-2 rounded-full mx-auto"
+                                :class="[
+                                    data?.calendar?.period.activeDays.some(
+                                        (d) =>
+                                            new Date(d).getMonth() ===
+                                                day.date.getMonth() &&
+                                            new Date(d).getDate() ===
+                                                day.date.getDate()
+                                    )
+                                        ? 'bg-gray'
+                                        : 'bg-none',
+                                ]"
+                            ></div>
                         </div>
                     </div>
                 </div>
@@ -119,7 +138,9 @@ const startEvent = (date: Date) => {
     const day = parseInt(isoDateString(date).substring(8, 10))
     for (const e of events) {
         //const start = isoDateString(new Date(e.start))
-        const start = parseInt(isoDateString(new Date(e.start)).substring(8, 10))
+        const start = parseInt(
+            isoDateString(new Date(e.start)).substring(8, 10)
+        )
         if (start === day) {
             return e.start
         }
@@ -149,7 +170,9 @@ const inEvent = (date: Date) => {
     for (const e of events) {
         /*const start = isoDateString(new Date(e.start))
         const end = isoDateString(new Date(e.end))*/
-        const start = parseInt(isoDateString(new Date(e.start)).substring(8, 10))
+        const start = parseInt(
+            isoDateString(new Date(e.start)).substring(8, 10)
+        )
         const end = parseInt(isoDateString(new Date(e.end)).substring(8, 10))
 
         if (start < day && end > day) {
