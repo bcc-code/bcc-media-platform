d1b2fe94f98e2efcb16c100055ddcbfea190a724
Fredrik Vedvik <fredrik@vedvik.tech>
2023-05-09 11:32:43 +0200

    fix(loader): validate that filter is not nil

--- backend/items/collection/loader.go
+++ backend/items/collection/loader.go
@@ -30,7 +30,10 @@ func NewCollectionItemLoader(ctx context.Context, db *sql.DB, collectionLoader *
 		if err == nil {
 			parallel.ForEach(collections, func(i *common.Collection, _ int) {
 				var identifiers []common.Identifier
-				if i.Type == "query" && i.Filter != nil {
+				if i.Type == "query" {
+					if i.Filter == nil {
+						i.Filter = &common.Filter{}
+					}
 					identifiers, err = GetItemIDsForFilter(ctx, db, roles, *i.Filter, i.AdvancedType.String == "continue_watching")
 					if err != nil {
 						log.L.Error().Err(err).
