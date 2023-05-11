6e666253f4bd4f998ac894b84099c656ae65671e
Fredrik Vedvik <fredrik@vedvik.tech>
2023-05-11 14:37:51 +0200

    fix video preview

--- packages/directus-modules/interface-video-viewer/src/viewer/Viewer.vue
+++ packages/directus-modules/interface-video-viewer/src/viewer/Viewer.vue
@@ -3,18 +3,6 @@
         <div>
             <Player v-if="asset" :asset="asset"></Player>
         </div>
-        <!-- <media-controller class="video-controller" v-if="asset">
-            
-            <media-control-bar>
-                <media-play-button></media-play-button>
-                <media-mute-button></media-mute-button>
-                <media-volume-range></media-volume-range>
-                <media-time-range></media-time-range>
-                <media-captions-button></media-captions-button>
-                <media-pip-button></media-pip-button>
-                <media-fullscreen-button></media-fullscreen-button>
-            </media-control-bar>
-        </media-controller> -->
         <div>
             <v-button @click="load" :loading="loading">Load</v-button>
             <div v-if="error">
@@ -45,6 +33,7 @@ const load = async () => {
     loading.value = true
     try {
         asset.value = await props.factory();
+        console.log(asset.value)
     } catch {
         error.value = "Couldn't retrieve from API"
     } finally {
