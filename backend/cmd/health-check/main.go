037ad777164b594a704fa970010141d4213670ba
Fredrik Vedvik <fredrik@vedvik.tech>
2023-06-20 09:23:03 +0200

    feat(health-check): query with app

--- backend/cmd/health-check/main.go
+++ backend/cmd/health-check/main.go
@@ -7,16 +7,76 @@ import (
 	"os"
 )
 
+//go:embed queries/application.graphql
+var applicationQuery string
+
 //go:embed queries/web.graphql
 var webQuery string
-var webQueryVariables map[string]string
+
+//go:embed queries/app.graphql
+var appQuery string
+
+//go:embed queries/tvos.graphql
+var tvosQuery string
+
+type applicationResult struct {
+	Application struct {
+		ClientVersion string
+		Page          struct {
+			Code string
+		}
+	}
+}
 
 func main() {
 	r := gin.Default()
 
 	r.GET("test", func(ctx *gin.Context) {
 		apiEndpoint := os.Getenv("API_ENDPOINT")
-		ctx.Status(executeQuery(apiEndpoint, webQuery, webQueryVariables))
+		var status int
+		var app applicationResult
+		status, app = executeQuery[applicationResult](apiEndpoint, applicationQuery, map[string]string{}, map[string]string{
+			"x-application": "btv",
+		})
+		if status != 200 {
+			ctx.Status(status)
+			return
+		}
+		status, app = executeQuery[applicationResult](apiEndpoint, webQuery, map[string]string{
+			"code": app.Application.Page.Code,
+		}, map[string]string{
+			"x-application": "btv",
+		})
+		if status != 200 {
+			ctx.Status(status)
+			return
+		}
+		status, app = executeQuery[applicationResult](apiEndpoint, appQuery, map[string]string{
+			"code": app.Application.Page.Code,
+		}, map[string]string{
+			"x-application": "btv",
+		})
+		if status != 200 {
+			ctx.Status(status)
+			return
+		}
+
+		status, app = executeQuery[applicationResult](apiEndpoint, applicationQuery, map[string]string{}, map[string]string{
+			"x-application": "tvos",
+		})
+		if status != 200 {
+			ctx.Status(status)
+			return
+		}
+		status, app = executeQuery[applicationResult](apiEndpoint, tvosQuery, map[string]string{
+			"code": app.Application.Page.Code,
+		}, map[string]string{
+			"x-application": "tvos",
+		})
+		if status != 200 {
+			ctx.Status(status)
+			return
+		}
 	})
 
 	_ = r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
--- backend/cmd/health-check/queries.go
+++ backend/cmd/health-check/queries.go
@@ -7,12 +7,12 @@ import (
 	"net/http"
 )
 
-type apiResult struct {
-	Data   any
+type apiResult[t any] struct {
+	Data   t
 	Errors any
 }
 
-func executeQuery(endpoint, query string, variables map[string]string) int {
+func executeQuery[t any](endpoint, query string, variables, headers map[string]string) (int, t) {
 	body, _ := json.Marshal(map[string]any{
 		"query":     query,
 		"variables": variables,
@@ -21,18 +21,22 @@ func executeQuery(endpoint, query string, variables map[string]string) int {
 	req, _ := http.NewRequest(http.MethodPost, endpoint, bytes.NewReader(body))
 
 	req.Header.Add("Content-Type", "application/json")
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	var result apiResult[t]
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.L.Error().Err(err).Send()
-		return 500
+		return 500, result.Data
 	}
 
-	var result apiResult
 	_ = json.NewDecoder(res.Body).Decode(&result)
 
 	if result.Errors != nil {
-		return 503
+		return 503, result.Data
 	}
-	return 200
+	return 200, result.Data
 }
