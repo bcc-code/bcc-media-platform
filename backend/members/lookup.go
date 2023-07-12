8a0e1f42aaffa9892e6cda1f5a1dffa2b5600cc9
Fredrik Vedvik <fredrik@vedvik.tech>
2023-07-12 11:55:05 +0200

    v2 persons

--- backend/members/lookup.go
+++ backend/members/lookup.go
@@ -30,7 +30,7 @@ func (c *Client) RetrieveByEmails(ctx context.Context, emails []string) (*[]Memb
 
 	encoded, _ := json.Marshal(filter)
 
-	return get[[]Member](ctx, c, fmt.Sprintf("persons?filter=%s,fields=%s", encoded, personFields))
+	return get[[]Member](ctx, c, fmt.Sprintf("v2/persons?filter=%s,fields=%s", encoded, personFields))
 }
 
 // GetMembersByIDs retrieves a batch of members by ID
