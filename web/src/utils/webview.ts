4766847d88c39521c957b4a93ebd4c3fd71ccb90
Fredrik Vedvik <fredrik@vedvik.tech>
2023-08-15 13:37:57 +0200

    in inframe dont run

--- web/src/utils/webview.ts
+++ web/src/utils/webview.ts
@@ -1,10 +1,9 @@
 import { WebView, WebViewCommunication, WebViewType } from "@/webview"
 
-
 class DelayedWebViewCommunication implements WebViewCommunication {
-    webviewPromise: Promise<WebViewCommunication>;
+    webviewPromise: Promise<WebViewCommunication>
     constructor(webviewPromise: Promise<WebViewCommunication>) {
-        this.webviewPromise = webviewPromise;
+        this.webviewPromise = webviewPromise
     }
 
     async callHandler(handlerName: string, ...args: any[]) {
@@ -18,57 +17,72 @@ function getWebView(): WebView | undefined {
         return {
             type: "flutter",
             communication: window.flutter_inappwebview,
-        };
+        }
     } else if (window.xamarin_webview) {
         return {
             type: "xamarin",
             communication: window.xamarin_webview,
-        };
+        }
     }
-    return undefined;
+    return undefined
 }
 
 function waitForWebview(): Promise<WebView> {
     return new Promise((resolve, reject) => {
         window.addEventListener("app_webview_ready", () => {
-            const webViewCommunication = getWebView();
+            const webViewCommunication = getWebView()
             if (webViewCommunication) {
-                resolve(webViewCommunication);
-            }
-            else {
-                reject();
+                resolve(webViewCommunication)
+            } else {
+                reject()
             }
         })
-    });
+    })
 }
 
 function getDelayedWebViewType(): WebViewType | undefined {
     const urlParams = new URLSearchParams(window.location.search)
-    const typeQueryParam = urlParams.get("webview_delayed_type")?.toLowerCase() as WebViewType | null;
+    const typeQueryParam = urlParams
+        .get("webview_delayed_type")
+        ?.toLowerCase() as WebViewType | null
     if (typeQueryParam) {
-        sessionStorage.setItem("webview_delayed_type", typeQueryParam);
-        return typeQueryParam;
+        sessionStorage.setItem("webview_delayed_type", typeQueryParam)
+        return typeQueryParam
     }
-    const storedType = sessionStorage.getItem("webview_delayed_type") as WebViewType | null;
-    return storedType ? storedType : undefined;
+    const storedType = sessionStorage.getItem(
+        "webview_delayed_type"
+    ) as WebViewType | null
+    return storedType ? storedType : undefined
 }
 
 function clearWebViewDataIfRequested() {
     const urlParams = new URLSearchParams(window.location.search)
-    const clearQueryParam = urlParams.get("webview_clear")?.toLowerCase() as any | null;
+    const clearQueryParam = urlParams.get("webview_clear")?.toLowerCase() as
+        | any
+        | null
     if (clearQueryParam == "true") {
-        sessionStorage.removeItem("webview_delayed_type");
+        sessionStorage.removeItem("webview_delayed_type")
     }
 }
 
-clearWebViewDataIfRequested();
-let currentWebView = getWebView();
-const delayedWebViewType = getDelayedWebViewType();
+function inIframe() {
+    try {
+        return window.self !== window.top
+    } catch (e) {
+        return true
+    }
+}
+
+clearWebViewDataIfRequested()
+let currentWebView = getWebView()
+const delayedWebViewType = !inIframe() ? getDelayedWebViewType() : false
 if (currentWebView == null && delayedWebViewType) {
     currentWebView = {
         type: delayedWebViewType,
-        communication: new DelayedWebViewCommunication(waitForWebview().then(webview => webview.communication)),
-    };
+        communication: new DelayedWebViewCommunication(
+            waitForWebview().then((webview) => webview.communication)
+        ),
+    }
 }
 
-export { currentWebView };
+export { currentWebView }
