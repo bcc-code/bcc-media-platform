99043b68073649bd9ad797332fb1c9dd21b4cd33
Fredrik Vedvik <fredrik@vedvik.tech>
2023-10-03 13:42:08 +0200

    make it possible to init from scratch

--- scripts/db_manage.sh
+++ scripts/db_manage.sh
@@ -45,17 +45,17 @@ set -o allexport
 source $SCRIPTPATH/../.env
 set +o allexport
 
-gum confirm "Really drop the btv database? Note that this will disconnect any connected users" && DB_RESET=true || DB_RESET=false
+gum confirm "Really drop the ${LOCALDB} database? Note that this will disconnect any connected users" && DB_RESET=true || DB_RESET=false
 if [[ $DB_RESET == true ]]; then
 	gum style --bold "DROP database..."
 
 	# We need to disconnect all users before we can drop the database
-	echo "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '${LOCALDB}' AND pid <> pg_backend_pid();" | psql -U btv -h localhost -d template1
+	echo "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '${LOCALDB}' AND pid <> pg_backend_pid();" | psql -U ${PGUSER} -h localhost -d template1
 	echo "DROP DATABASE IF EXISTS ${LOCALDB};" | psql -h localhost -d template1
 	echo ":heavy_check_mark:  Database dropped" | gum format -t emoji
 fi
 
-for f in $(find /Users/matjaz/prog/bcc.media/brunstadtv/scripts/../migrations/special/pre -name \*.sql | sort -V); do
+for f in $(find ./migrations/special/pre -name \*.sql | sort -V); do
 	gum style --foreground 212 --border-foreground 212 --border double --align center --width 100 --margin "1 1" --padding "1 1" "$(head -n1 $f | cut -c 4-)"
 	gum confirm "Run ${f}?" && RUN=true || RUN=false
 	if [[ $RUN == true ]]; then
@@ -63,10 +63,10 @@ for f in $(find /Users/matjaz/prog/bcc.media/brunstadtv/scripts/../migrations/sp
 	fi
 done
 
-gum confirm "Really create the btv database?" && RUN=true || RUN=false
+gum confirm "Really create the ${LOCALDB} database?" && RUN=true || RUN=false
 if [[ $RUN == true ]]; then
 	gum style --bold "Create database..."
-	echo "CREATE DATABASE btv WITH OWNER = manager ENCODING = 'UTF8' CONNECTION LIMIT = -1 IS_TEMPLATE = False;" | psql -h localhost -d template1
+	echo "CREATE DATABASE ${LOCALDB} WITH OWNER = manager ENCODING = 'UTF8' CONNECTION LIMIT = -1 IS_TEMPLATE = False;" | psql -h localhost -d template1
 	echo ":heavy_check_mark:  Database created" | gum format -t emoji
 fi
 
@@ -78,7 +78,7 @@ if [[ $RUN == true ]]; then
 	cd -
 fi
 
-for f in $(find /Users/matjaz/prog/bcc.media/brunstadtv/scripts/../migrations/special/post -name \*.sql | sort -V); do
+for f in $(find ./migrations/special/post -name \*.sql | sort -V); do
 	gum style --foreground 212 --border-foreground 212 --border double --align center --width 100 --margin "1 1" --padding "1 1" "$(head -n1 $f | cut -c 4-)"
 	gum confirm "Run ${f}?" && RUN=true || RUN=false
 	if [[ $RUN == true ]]; then
