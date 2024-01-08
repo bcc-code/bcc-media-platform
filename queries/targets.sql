8fad1d57ffbed090e14680d7418e7956fdd76c9e
Fredrik Vedvik <fredrik@vedvik.tech>
2024-01-08 13:46:52 +0100

    fix: whitespace

--- queries/targets.sql
+++ queries/targets.sql
@@ -4,7 +4,7 @@ WITH groups AS (SELECT targets_id, array_agg(usergroups_code)::varchar[] as code
                 GROUP BY targets_id)
 SELECT t.id,
        t.type,
-       g.codes                                                                         AS group_codes
+       g.codes AS group_codes
 FROM targets t
          LEFT JOIN groups g ON g.targets_id = t.id
 WHERE id = ANY ($1::uuid[]);
