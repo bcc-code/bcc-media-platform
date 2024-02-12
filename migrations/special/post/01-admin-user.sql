50e767bae586024cffa72f19ac5d85796e24cc00
Andreas Gangsø <andreasgangso@gmail.com>
2024-02-12 11:40:58 +0100

    chore: fix dev first time setup

--- migrations/special/post/01-admin-user.sql
+++ migrations/special/post/01-admin-user.sql
@@ -1,6 +1,6 @@
 -- Create the default directus admin user
 INSERT INTO public.directus_users (id, first_name, last_name, email, password, location, title, description, tags,
-                                   avatar, language, theme, tfa_secret, status, role, token, last_access, last_page,
+                                   avatar, language, theme_light, tfa_secret, status, role, token, last_access, last_page,
                                    provider, external_identifier, auth_data, email_notifications)
 VALUES ('45ac012b-185d-4c93-8a16-6ba7d51d261c', 'Admin', 'User', 'admin@brunstad.tv',
         '$argon2id$v=19$m=4096,t=3,p=1$5R/76zRWiBA6yxuHuVjLnw$XV40T3YN4qXi4YSt02MQXasmLZHRxE1PMX2w1CgKGXE', NULL, NULL,
