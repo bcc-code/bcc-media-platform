7b46eea0a1f5931331f4953372607266dd2914d9
Andreas Gangsø <andreasgangso@gmail.com>
2024-03-07 11:36:01 +0100

    feat: quotes

---
+++ web/src/components/quotes/Quote.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps<{
+    author: string
+    quote: string
+}>()
+</script>
+<template>
+    <div
+        class="inline-flex flex-col items-center justify-center w-full h-full bg-background-2 rounded-2xl"
+    >
+        <div class="p-4 text-style-body-2 text-center">«{{ quote }}»</div>
+        <div>{{ author }}</div>
+    </div>
+</template>
--- web/src/components/quotes/QuoteOfTheDay.vue
+++ web/src/components/quotes/QuoteOfTheDay.vue
@@ -1,18 +1,55 @@
 <script setup lang="ts">
-defineProps<{
-    comicId: string
-}>()
+import Quote from "./Quote.vue"
+import quotesRaw from "../../utils/quotes/all.json"
+import { useI18n } from "vue-i18n"
+import { useAsyncState } from "@vueuse/core"
+import { computed } from "vue"
+
+type Quote = (typeof quotesRaw)[keyof typeof quotesRaw]
+const quotes = quotesRaw as Record<string, Quote>
+
+type LocalizedModule = {
+    default: Record<string, string>
+}
+const localized = import.meta.glob<LocalizedModule>(
+    "../../utils/quotes/localized/*.json"
+)
+
+const today = new Date()
+
+let quote = quotes[Math.floor(Math.random() * Object.keys(quotes).length)]
+for (const key in quotes) {
+    const val = quotes[key]
+    //published_at: 07/03/2024 for example, but that's day/month/year
+    const publishedAt = new Date(
+        val.published_at.split("/").reverse().join("/")
+    )
+    if (
+        publishedAt.getDate() === today.getDate() &&
+        publishedAt.getMonth() === today.getMonth() &&
+        publishedAt.getFullYear() === today.getFullYear()
+    ) {
+        quote = val
+        break
+    }
+}
+
+const { locale } = useI18n()
+
+let localizedQuotesPromise =
+    localized[`../../utils/quotes/localized/${locale.value}.json`]?.()
+
+const { isLoading, state: localizedQuotes } = useAsyncState(
+    localizedQuotesPromise,
+    undefined
+)
+
+const quoteText = computed(() => {
+    return localizedQuotes.value?.default[quote.id] ?? quote.quote
+})
 </script>
 
 <template>
-    <div
-        class="inline-flex flex-col items-center justify-center w-full h-full bg-background-2 rounded-2xl"
-    >
-        <div class="p-4 text-style-body-2 text-center">
-            Guds Ånd er ikke avhengig av inngrodde skikker og seremonier, for
-            den er lettbevegelig, og den gjør dem lettbevegelig som dannes av
-            den. Visdommen er den som skal regjere evindelig.
-        </div>
-        <div>Johan O. Smith</div>
-    </div>
+    <div v-if="isLoading || !quote"></div>
+    <Quote v-else-if="quote" :quote="quoteText" :author="quote.author" />
 </template>
