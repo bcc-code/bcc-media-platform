9b5a28af273e45d8c388229607435feb158d08c9
Andreas Gangsø <andreasgangso@gmail.com>
2024-03-07 15:46:54 +0100

    feat: quote sharing, etc

--- web/src/components/quotes/Quote.vue
+++ web/src/components/quotes/Quote.vue
@@ -5,10 +5,11 @@ defineProps<{
 }>()
 </script>
 <template>
-    <div
-        class="inline-flex flex-col items-center justify-center w-full h-full bg-background-2 rounded-2xl"
-    >
-        <div class="p-4 text-style-body-2 text-center">«{{ quote }}»</div>
-        <div>{{ author }}</div>
+    <div class="text-left flex">
+        <div class="w-0.5 rounded-full bg-white shrink-0"></div>
+        <div class="pl-3 pb-2">
+            <div class="text-style-body-1">«{{ quote }}»</div>
+            <div class="pt-2 text-style-body-2">{{ author }}</div>
+        </div>
     </div>
 </template>
--- web/src/components/quotes/QuoteOfTheDay.vue
+++ web/src/components/quotes/QuoteOfTheDay.vue
@@ -3,7 +3,9 @@ import Quote from "./Quote.vue"
 import quotesRaw from "../../utils/quotes/all.json"
 import { useI18n } from "vue-i18n"
 import { useAsyncState } from "@vueuse/core"
-import { computed } from "vue"
+import { computed, ref, watch, watchEffect } from "vue"
+import VButton from "../VButton.vue"
+import Check from "../icons/Check.vue"
 
 type Quote = (typeof quotesRaw)[keyof typeof quotesRaw]
 const quotes = quotesRaw as Record<string, Quote>
@@ -47,9 +49,90 @@ const { isLoading, state: localizedQuotes } = useAsyncState(
 const quoteText = computed(() => {
     return localizedQuotes.value?.default[quote.id] ?? quote.quote
 })
+
+const todayString = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+const readDaysArray = ref(localStorage.getItem("read_days")?.split(",") ?? [])
+watch(readDaysArray, () => {
+    localStorage.setItem("read_days", readDaysArray.value.join(","))
+})
+const setRead = (val: boolean) => {
+    if (!val) {
+        readDaysArray.value = readDaysArray.value.filter(
+            (day) => day !== todayString
+        )
+    } else {
+        readDaysArray.value = [...readDaysArray.value, todayString]
+    }
+}
+const hasReadToday = computed(() => readDaysArray.value.includes(todayString))
+
+const share = () => {
+    if (navigator.share) {
+        navigator.share({
+            title: "Quote of the day",
+            text: `«${quoteText.value}» - ${quote.author}`,
+            url: window.location.href,
+        })
+    }
+}
 </script>
 
 <template>
-    <div v-if="isLoading || !quote"></div>
-    <Quote v-else-if="quote" :quote="quoteText" :author="quote.author" />
+    <div
+        class="flex flex-col items-start justify-start w-full py-4 bg-background-2 rounded-2xl hide-scrollbar"
+        style="max-height: 100%; overflow-y: auto"
+        @click="setRead(true)"
+    >
+        <div v-if="isLoading || !quote"></div>
+        <Quote
+            v-else-if="quote"
+            :quote="quoteText"
+            :author="quote.author"
+            class="mx-5"
+        />
+        <div class="flex gap-2 mx-5 mt-4">
+            <VButton
+                size="thin"
+                :color="hasReadToday ? 'default' : 'secondary'"
+                class="bg-background-1 rounded-full"
+                @click.stop="setRead(!hasReadToday)"
+                :aria-label="
+                    hasReadToday
+                        ? $t('quotes.markAsUnread')
+                        : $t('quotes.markAsRead')
+                "
+            >
+                <Check />
+            </VButton>
+            <VButton
+                color="secondary"
+                size="thin"
+                class="flex items-center"
+                @click="share"
+            >
+                <svg
+                    class="mr-1 h-6 w-6"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M3 13L3 18C3 19.6569 4.34315 21 6 21L18 21C19.6569 21 21 19.6569 21 18L21 13L19 13L19 18C19 18.5523 18.5523 19 18 19L6 19C5.44772 19 5 18.5523 5 18L5 13L3 13Z"
+                        fill="currentColor"
+                    />
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M16.9246 7.92463L13.407 4.40704L12 3L10.5786 4.42139L7.02515 7.97486L8.43218 9.3819L11 6.81408V15H13V6.84278L15.5033 9.34602L16.9246 7.92463Z"
+                        fill="currentColor"
+                    />
+                </svg>
+                {{ $t("share.title") }}
+            </VButton>
+        </div>
+    </div>
 </template>
