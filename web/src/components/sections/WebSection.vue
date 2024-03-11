b05a1e25a6bd2c5e017c54223b4382820f67949a
Andreas Gangsø <andreasgangso@gmail.com>
2024-03-11 11:49:24 +0100

    feat(web): websection and /w/ routes

--- web/src/components/sections/WebSection.vue
+++ web/src/components/sections/WebSection.vue
@@ -1,39 +1,22 @@
 <template>
     <section>
         <SectionTitle v-if="item.title">{{ item.title }}</SectionTitle>
-        <div class="w-full">
-            <iframe
-                ref="frame"
-                class="w-full"
-                :src="item.url"
-                :height="effectiveHeight"
-            ></iframe>
+        <div
+            class="w-full md:w-1/2 xl:w-1/4 xl:min-w-[400px]"
+            :style="{
+                aspectRatio: item.aspectRatio ?? undefined,
+                height: item.height ?? undefined,
+            }"
+        >
+            <iframe ref="frame" class="w-full h-full" :src="item.url"></iframe>
         </div>
     </section>
 </template>
 <script lang="ts" setup>
 import { Section } from "./types"
 import SectionTitle from "./item/SectionTitle.vue"
-import { computed, ref } from "vue"
 
-const props = defineProps<{
+defineProps<{
     item: Section & { __typename: "WebSection" }
 }>()
-
-const frame = ref(null as HTMLIFrameElement | null)
-
-const effectiveHeight = computed(() => {
-    if (props.item.height) {
-        return props.item.height
-    }
-    if (!frame.value) {
-        return 0
-    }
-    const rect = frame.value?.getBoundingClientRect()
-    if (!rect) {
-        return 0
-    }
-
-    return rect.width / (props.item.aspectRatio ?? 1)
-})
 </script>
