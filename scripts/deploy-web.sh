4fc9fa35fe33cfbc79271efe19ff956b6f2a0c2a
Fredrik Vedvik <fredrik@vedvik.tech>
2024-03-14 13:01:46 +0100

    fix: cache on web

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -10,4 +10,4 @@ artifact pull workflow build-$ENV
 gsutil -m cp -r build-$ENV/* gs://btv-web-$ENV-2
 
 # Avoid cache on index.html
-gsutil setmeta -h "Cache-Control:max-age=60" gs://btv-web-$ENV-2/index.html
+gsutil setmeta -h "Cache-Control:max-age=60,must-revalidate" gs://btv-web-$ENV-2/index.html
