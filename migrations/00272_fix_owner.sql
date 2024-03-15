527cdd09568d617848c7e09317ee9460f5b62d8f
Fredrik Vedvik <fredrik@vedvik.tech>
2024-03-15 12:03:34 +0100

    .

--- migrations/00272_fix_owner.sql
+++ migrations/00272_fix_owner.sql
@@ -4,11 +4,11 @@
 /***    CREATED ON: 2024-03-14T14:37:31.016Z             ***/
 /***********************************************************/
 
-alter materialized view public.filter_dataset owner to background_worker;
 
 grant all on sequence events_translations_id_seq to background_worker;
 grant all on sequence mediaitems_translations_id_seq to background_worker;
 GRANT ALL ON SEQUENCE calendarentries_translations_id_seq TO background_worker;
+alter materialized view public.filter_dataset owner to builder;
 GRANT SELECT ON games_usergroups TO background_worker;
 
 -- +goose Down
