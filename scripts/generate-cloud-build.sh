fe387e558a689d40b07d7ff34187557fee987d1e
Andreas Gangsø <andreasgangso@gmail.com>
2024-06-07 13:12:00 +0200

    revert whitespace changes

--- scripts/generate-cloud-build.sh
+++ scripts/generate-cloud-build.sh
@@ -5,34 +5,34 @@ ROUTE="run-route.yaml"
 M="migrations.yaml"
 
 function deploy {
-	echo "- name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'" >>$CB
-	echo "  waitFor: ['-']" >>$CB
-	echo "  entrypoint: gcloud" >>$CB
-	echo "  args: ['run', 'deploy', '\$_${1}_SERVICE', '--project', '\$_PROJECT_ID', '--no-traffic', '--image', '$(cat ${2})', '--region', 'europe-west4']" >>$CB
+	echo "- name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'" >> $CB
+	echo "  waitFor: ['-']" >> $CB
+	echo "  entrypoint: gcloud" >> $CB
+	echo "  args: ['run', 'deploy', '\$_${1}_SERVICE', '--project', '\$_PROJECT_ID', '--no-traffic', '--image', '$(cat ${2})', '--region', 'europe-west4']" >> $CB
 }
 
 function route {
-	echo "- name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'" >>$ROUTE
-	echo "  waitFor: ['-']" >>$ROUTE
-	echo "  entrypoint: gcloud" >>$ROUTE
-	echo "  args: ['run', 'services',  'update-traffic', '\$_${1}_SERVICE', '--to-latest', '--region=europe-west4']" >>$ROUTE
+	echo "- name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'" >> $ROUTE
+	echo "  waitFor: ['-']" >> $ROUTE
+	echo "  entrypoint: gcloud" >> $ROUTE
+	echo "  args: ['run', 'services',  'update-traffic', '\$_${1}_SERVICE', '--to-latest', '--region=europe-west4']" >> $ROUTE
 }
 
 function migrations {
-	echo "- name: ${1}" >>$M
-	echo "  args: []" >>$M
-	echo "  env:" >>$M
-	echo "    - 'INSTANCE_CONNECTION_NAME=\$_INSTANCE_CONNECTION_NAME'" >>$M
-	echo "  secretEnv: ['PGPASSWORD']" >>$M
-	echo "availableSecrets:" >>$M
-	echo "  secretManager:" >>$M
-	echo "  - versionName: projects/\$PROJECT_ID/secrets/postgres_builder_password/versions/latest" >>$M
-	echo "    env: 'PGPASSWORD'" >>$M
+	echo "- name: ${1}" >> $M
+	echo "  args: []" >> $M
+	echo "  env:" >> $M
+	echo "    - 'INSTANCE_CONNECTION_NAME=\$_INSTANCE_CONNECTION_NAME'" >> $M
+	echo "  secretEnv: ['PGPASSWORD']" >> $M
+	echo "availableSecrets:" >> $M
+	echo "  secretManager:" >> $M
+	echo "  - versionName: projects/\$PROJECT_ID/secrets/postgres_builder_password/versions/latest" >> $M
+	echo "    env: 'PGPASSWORD'"  >> $M
 }
 
 function allow_loose {
-	echo "options:" >>$1
-	echo "   substitution_option: 'ALLOW_LOOSE'" >>$1
+	echo "options:" >> $1
+	echo "   substitution_option: 'ALLOW_LOOSE'" >> $1
 }
 
 artifact pull workflow api.txt || true
@@ -42,7 +42,7 @@ artifact pull workflow cms.txt || true
 artifact pull workflow migrations.txt
 
 # We always have migrations
-echo "steps:" >$M
+echo "steps:" > $M
 migrations $(cat migrations.txt)
 artifact push workflow $M
 
@@ -52,10 +52,10 @@ if [ ! -f "api.txt" ] && [ ! -f "jobs.txt" ] && [ ! -f "cms.txt" ] && [ ! -f "re
 fi
 
 allow_loose $CB
-echo "steps:" >>$CB
+echo "steps:" >> $CB
 
 allow_loose $ROUTE
-echo "steps:" >>$ROUTE
+echo "steps:" >> $ROUTE
 
 if [ -f "api.txt" ]; then
 	deploy API api.txt
