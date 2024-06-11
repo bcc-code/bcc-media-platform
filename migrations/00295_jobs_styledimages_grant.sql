f64394a451842a2f9741a562264a53b27e49433e
Andreas Gangsø <andreasgangso@gmail.com>
2024-06-11 08:07:54 +0200

    fix down migration [ci skip]

--- migrations/00295_jobs_styledimages_grant.sql
+++ migrations/00295_jobs_styledimages_grant.sql
@@ -10,9 +10,9 @@ GRANT DELETE ON TABLE "public"."timedmetadata_styledimages" TO background_worker
 -- +goose Down
 REVOKE INSERT ON TABLE "public"."styledimages" FROM background_worker;
 REVOKE UPDATE ON TABLE "public"."styledimages" FROM background_worker;
-REVOKE DELETE ON TABLE "public"."styledimages" TO background_worker;
+REVOKE DELETE ON TABLE "public"."styledimages" FROM background_worker;
 
 REVOKE INSERT ON TABLE "public"."timedmetadata_styledimages" FROM background_worker;
 REVOKE UPDATE ON TABLE "public"."timedmetadata_styledimages" FROM background_worker;
-REVOKE DELETE ON TABLE "public"."timedmetadata_styledimages" TO background_worker;
+REVOKE DELETE ON TABLE "public"."timedmetadata_styledimages" FROM background_worker;
 
