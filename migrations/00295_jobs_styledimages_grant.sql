5c26f9ee22624567a843dc9e580b6462c9039c61
Andreas Gangsø <andreasgangso@gmail.com>
2024-06-11 08:01:56 +0200

    fix(jobs): grant access to styledimages db tables

---
+++ migrations/00295_jobs_styledimages_grant.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+GRANT INSERT ON TABLE "public"."styledimages" TO background_worker;
+GRANT UPDATE ON TABLE "public"."styledimages" TO background_worker;
+GRANT DELETE ON TABLE "public"."styledimages" TO background_worker;
+
+GRANT INSERT ON TABLE "public"."timedmetadata_styledimages" TO background_worker;
+GRANT UPDATE ON TABLE "public"."timedmetadata_styledimages" TO background_worker;
+GRANT DELETE ON TABLE "public"."timedmetadata_styledimages" TO background_worker;
+
+-- +goose Down
+REVOKE INSERT ON TABLE "public"."styledimages" FROM background_worker;
+REVOKE UPDATE ON TABLE "public"."styledimages" FROM background_worker;
+REVOKE DELETE ON TABLE "public"."styledimages" TO background_worker;
+
+REVOKE INSERT ON TABLE "public"."timedmetadata_styledimages" FROM background_worker;
+REVOKE UPDATE ON TABLE "public"."timedmetadata_styledimages" FROM background_worker;
+REVOKE DELETE ON TABLE "public"."timedmetadata_styledimages" TO background_worker;
+
