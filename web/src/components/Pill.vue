23a9637650d6f9a0d07bd5d7337790ed345ce00b
Sigve Hansen <sighan12@gmail.com>
2024-08-12 12:08:29 +0200

    chore: move pill component

---
+++ web/src/components/Pill.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+    color?: string
+}>()
+</script>
+
+<template>
+    <span
+        class="rounded-xl text-sm px-2 z-10 font-medium"
+        :class="color ?? 'bg-slate-800'"
+    >
+        <slot />
+    </span>
+</template>
+w
