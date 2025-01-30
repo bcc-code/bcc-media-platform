d882e6b763b5d1305dbb15131129fa1604b0c1c3
Matjaz Debelak <matjaz.debelak@bcc.no>
2025-01-30 16:23:42 +0100

    Fix wrong join for calendar entries translations

--- queries/calendar.sql
+++ queries/calendar.sql
@@ -100,7 +100,8 @@ WHERE e.id = ANY ($1::int[]);
 -- name: GetCalendarEntriesTranslatable :many
 SELECT e.id, title, description
 FROM calendarentries_translations et
-         JOIN events e ON e.id = et.calendarentries_id
+    JOIN calendarentries e ON e.id = et.calendarentries_id
 WHERE et.languages_code ='no'
   AND et.date_updated > @date_updated
-  and e.status = ANY ('{published,unlisted}');
+  AND e.status = ANY ('{published,unlisted}');
+
