49b42483d6b9910f2fe8d1076aae404261252583
Matjaz Debelak <matjaz.debelak@bcc.no>
2025-02-03 22:49:03 +0100

    Fix main

--- backend/cmd/jobs/main.go
+++ backend/cmd/jobs/main.go
@@ -73,7 +73,7 @@ func main() {
 	eventHandler := events.NewHandler()
 	statisticsHandler := statistics.NewHandler(ctx, config.BigQuery, queries)
 
-	phraseClient := phrase.NewClient(rdb, "", config.Phrase.Username, config.Phrase.Password, config.Phrase.ProjectUID, config.Phrase.CallbackURL)
+	phraseClient := phrase.NewClient(rdb, config.Phrase)
 	err = phraseClient.Authenticate()
 	if err != nil {
 		log.L.Panic().Err(err).Msg("Failed to authenticate phrase")
