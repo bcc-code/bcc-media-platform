458bd759d7fc2fb035e376628aa20920da5c06cf
Sigve Hansen <sighan12@gmail.com>
2025-03-07 12:57:32 +0100

    fix: try overriding the flutter_webview_manager to inject logging

--- web/src/utils/webview.ts
+++ web/src/utils/webview.ts
@@ -1,3 +1,4 @@
+import { analytics } from '@/services/analytics'
 import { WebView, WebViewCommunication, WebViewType } from '@/webview'
 
 class DelayedWebViewCommunication implements WebViewCommunication {
@@ -35,6 +36,46 @@ function getWebView(): WebView | undefined {
 function waitForWebview(): Promise<WebView> {
     return new Promise((resolve, reject) => {
         window.addEventListener('app_webview_ready', () => {
+            // Override webview communication to log errors
+            window.flutter_webview_manager = {
+                // @ts-expect-error ignore
+                // eslint-disable-next-line no-shadow-restricted-names
+                callHandler: async (name, ...arguments) => {
+                    const requestId = Math.random().toString(36).substring(7);
+                    // @ts-expect-error ignore
+                    window.flutter_channel.postMessage(JSON.stringify({
+                        requestId,
+                        name,
+                        arguments,
+                    }));
+
+                    // @ts-expect-error ignore
+                    while (!(requestId in window.flutterResponses)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    // @ts-expect-error ignore
+                    const data = window.flutterResponses[requestId];
+                    // @ts-expect-error ignore
+                    delete window.flutterResponses[requestId];
+                    if (data instanceof Error) {
+                        throw data;
+                    }
+                    return data;
+                },
+                // @ts-expect-error ignore
+                respond: (id, result) => {
+                    // @ts-expect-error ignore
+                    window.flutterResponses[id] = result;
+                },
+                // @ts-expect-error ignore
+                respondError: (id, error) => {
+                    // @ts-expect-error ignore
+                    window.flutterResponses[id] = new Error(error);
+                    analytics.track('error', { message: 'Error calling study handler', data: { id, error } })
+                }
+            };
+            // Override webview communication to log errors end
+
             const webViewCommunication = getWebView()
             if (webViewCommunication) {
                 resolve(webViewCommunication)
