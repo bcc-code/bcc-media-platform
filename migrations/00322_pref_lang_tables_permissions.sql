440dbfec662913e4a8f53eb274bb6c9ce8a08c57
Matjaz Debelak <matjaz.debelak@bcc.no>
2025-06-10 12:13:42 +0200

    No comment

--- migrations/00322_pref_lang_tables_permissions.sql
+++ migrations/00322_pref_lang_tables_permissions.sql
@@ -1,16 +1,12 @@
 -- +goose Up
-/**********************************************************/
-/*** SCRIPT AUTHOR: CASCADE AI                          ***/
-/***    CREATED ON: 2025-06-10T12:01:20+02:00           ***/
-/**********************************************************/
 
 -- Grant SELECT permissions to api user on new tables
 GRANT SELECT ON "public"."applicationgroups_languages" TO api;
 GRANT SELECT ON "public"."applicationgroups_languages_subs" TO api;
 
--- Grant SELECT permissions to background user on new tables
-GRANT SELECT ON "public"."applicationgroups_languages" TO background;
-GRANT SELECT ON "public"."applicationgroups_languages_subs" TO background;
+-- Grant SELECT permissions to background_worker on new tables
+GRANT SELECT ON "public"."applicationgroups_languages" TO background_worker;
+GRANT SELECT ON "public"."applicationgroups_languages_subs" TO background_worker;
 
 -- Grant SELECT, INSERT, UPDATE permissions to directus user on new tables
 GRANT SELECT, INSERT, UPDATE ON "public"."applicationgroups_languages" TO directus;
@@ -21,18 +17,14 @@ GRANT USAGE, SELECT ON SEQUENCE "public"."applicationgroups_languages_id_seq" TO
 GRANT USAGE, SELECT ON SEQUENCE "public"."applicationgroups_languages_subs_id_seq" TO directus;
 
 -- +goose Down
-/**********************************************************/
-/*** SCRIPT AUTHOR: CASCADE AI                          ***/
-/***    CREATED ON: 2025-06-10T12:01:20+02:00           ***/
-/**********************************************************/
 
 -- Revoke permissions from api user
 REVOKE SELECT ON "public"."applicationgroups_languages" FROM api;
 REVOKE SELECT ON "public"."applicationgroups_languages_subs" FROM api;
 
--- Revoke permissions from background user 
-REVOKE SELECT ON "public"."applicationgroups_languages" FROM background;
-REVOKE SELECT ON "public"."applicationgroups_languages_subs" FROM background;
+-- Revoke permissions from background_worker
+REVOKE SELECT ON "public"."applicationgroups_languages" FROM background_worker;
+REVOKE SELECT ON "public"."applicationgroups_languages_subs" FROM background_worker;
 
 -- Revoke permissions from directus user
 REVOKE SELECT, INSERT, UPDATE ON "public"."applicationgroups_languages" FROM directus;
