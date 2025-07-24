9c49fecf3ae36ef4d0891273a129fc1caec77bc2
Matjaz Debelak <matjaz.debelak@bcc.no>
2025-07-24 13:44:13 +0200

    Remove redundant import

--- backend/graph/api/faq.resolvers.go
+++ backend/graph/api/faq.resolvers.go
@@ -10,7 +10,6 @@ import (
 	"github.com/bcc-code/bcc-media-platform/backend/cursors"
 	"github.com/bcc-code/bcc-media-platform/backend/graph/api/generated"
 	"github.com/bcc-code/bcc-media-platform/backend/graph/api/model"
-	"github.com/bcc-code/bcc-media-platform/backend/log"
 	"github.com/bcc-code/bcc-media-platform/backend/utils"
 	"github.com/google/uuid"
 )
