8e9ebaa404c8b302b1e243741fd2cc11335beda0
Matjaz Debelak <matjaz.debelak@bcc.no>
2025-09-10 14:52:00 +0200

    Add lots of sendgrid logging

--- backend/email/send.go
+++ backend/email/send.go
@@ -2,6 +2,8 @@ package email
 
 import (
 	"context"
+	"fmt"
+	"github.com/bcc-code/bcc-media-platform/backend/log"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"go.opentelemetry.io/otel"
@@ -32,6 +34,59 @@ func (s *Service) SendEmail(ctx context.Context, options SendOptions) error {
 
 	message.ReplyTo = from
 
-	_, err := s.client.Send(message)
-	return err
+	// Log the email attempt
+	log.L.Info().
+		Str("from_email", options.From.Email).
+		Str("from_name", options.From.Name).
+		Str("to_email", options.To.Email).
+		Str("to_name", options.To.Name).
+		Str("title", options.Title).
+		Msg("Attempting to send email via SendGrid")
+
+	response, err := s.client.Send(message)
+	
+	// Log all SendGrid responses for debugging
+	if response != nil {
+		log.L.Info().
+			Int("status_code", response.StatusCode).
+			Str("response_body", response.Body).
+			Interface("headers", response.Headers).
+			Str("from_email", options.From.Email).
+			Str("to_email", options.To.Email).
+			Str("title", options.Title).
+			Msg("SendGrid API response")
+	}
+
+	// Handle errors
+	if err != nil {
+		log.L.Error().
+			Err(err).
+			Str("from_email", options.From.Email).
+			Str("to_email", options.To.Email).
+			Str("title", options.Title).
+			Msg("SendGrid client error")
+		return err
+	}
+
+	// Check for HTTP error status codes
+	if response != nil && response.StatusCode >= 400 {
+		errorMsg := fmt.Sprintf("SendGrid API error: %d - %s", response.StatusCode, response.Body)
+		log.L.Error().
+			Int("status_code", response.StatusCode).
+			Str("response_body", response.Body).
+			Str("from_email", options.From.Email).
+			Str("to_email", options.To.Email).
+			Str("title", options.Title).
+			Msg("SendGrid API returned error status")
+		return fmt.Errorf(errorMsg)
+	}
+
+	// Log successful sends
+	log.L.Info().
+		Str("from_email", options.From.Email).
+		Str("to_email", options.To.Email).
+		Str("title", options.Title).
+		Msg("Email sent successfully via SendGrid")
+
+	return nil
 }
