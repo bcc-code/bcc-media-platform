33cbdde0b98b07cb5305b8cb35857fef23fcb932
Sigve Hansen <sighan12@gmail.com>
2026-05-06 09:48:34 +0200

    chore: update major packages

--- packages/video-player/src/video-player/utils/userAgent.ts
+++ packages/video-player/src/video-player/utils/userAgent.ts
@@ -1,10 +1,10 @@
-import UAParser from "ua-parser-js"
+import { UAParser } from "ua-parser-js"
 
 const _ua = new UAParser().getResult()
 
 /*
 
-To simulate Samsung SmartTV, pass this in the constructor: 
+To simulate Samsung SmartTV, pass this in the constructor:
 Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1
 
 */
